e3ba96716c004287ec54fa3e64e6c32a0aeac06b
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-07-31 17:42:57 -0300

    IMP journal document config

--- l10n_ar_invoice/wizard/journal_config_wizard.py
+++ l10n_ar_invoice/wizard/journal_config_wizard.py
@@ -28,38 +28,42 @@ class account_journal_document_config(osv.osv_memory):
 
         journal_ids = context.get('active_ids', False)
         wizard = self.browse(cr, uid, ids[0], context=context)
-        for journal in self.pool['account.journal'].browse(cr, uid, journal_ids, context=context):
-          responsability = journal.company_id.responsability_id
-          if not responsability.id:
-              raise orm.except_orm(_('Your company has not setted any responsability'),
-                      _('Please, set your company responsability in the company partner before continue.'))            
-              _logger.warning('Your company "%s" has not setted any responsability.' % journal.company_id.name)
+        self.create_journals(cr, uid, wizard.debit_notes, wizard.credit_notes, journal_ids, context=context)
+
+    def create_journals(self, cr, uid, debit_notes, credit_notes, journal_ids, context=None):
+      print 'sdas', debit_notes, credit_notes, journal_ids, context
+      for journal in self.pool['account.journal'].browse(cr, uid, journal_ids, context=context):
+        responsability = journal.company_id.responsability_id
+        if not responsability.id:
+            raise orm.except_orm(_('Your company has not setted any responsability'),
+                    _('Please, set your company responsability in the company partner before continue.'))            
+            _logger.warning('Your company "%s" has not setted any responsability.' % journal.company_id.name)
+      
+        journal_type = journal.type
+        if journal_type in ['sale', 'sale_refund']:
+          letter_ids = [x.id for x in responsability.issued_letter_ids]
+        elif journal_type in ['purchase', 'purchase_refund']:
+          letter_ids = [x.id for x in responsability.received_letter_ids]
         
-          journal_type = journal.type
-          if journal_type in ['sale', 'sale_refund']:
-            letter_ids = [x.id for x in responsability.issued_letter_ids]
-          elif journal_type in ['purchase', 'purchase_refund']:
-            letter_ids = [x.id for x in responsability.received_letter_ids]
-          
-          if journal_type == 'sale':
-            document_type = 'invoice'            
-            self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)
-            # Create sale debit notes
-            if wizard.debit_notes != 'dont_use':
-              document_type = 'debit_note'            
-              self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)
-          elif journal_type == 'sale_refund':
-            # Create sale credit notes
-              document_type = 'credit_note'            
-              self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)
-          elif journal_type == 'purchase':
-            document_type = 'invoice'
-            self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)
+        if journal_type == 'sale':
+          document_type = 'invoice'            
+          self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)
+          # Create sale debit notes
+          if debit_notes != 'dont_use':
             document_type = 'debit_note'            
-            self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)            
-          elif journal_type == 'purchase_refund':
-            document_type = 'credit_note'
-            self.create_journal_document(cr, uid, letter_ids, document_type, journal, wizard, context)
+            self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)
+        elif journal_type == 'sale_refund':
+          # Create sale credit notes
+            document_type = 'credit_note'            
+            self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)
+        elif journal_type == 'purchase':
+          document_type = 'invoice'
+          self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)
+          document_type = 'debit_note'            
+          self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)            
+        elif journal_type == 'purchase_refund':
+          document_type = 'credit_note'
+          self.create_journal_document(cr, uid, letter_ids, document_type, journal.id, credit_notes, debit_notes, context)
 
     def create_sequence(self, cr, uid, name, journal, context=None):
       vals = {
@@ -70,27 +74,31 @@ class account_journal_document_config(osv.osv_memory):
       sequence_id = self.pool['ir.sequence'].create(cr, uid, vals, context=context)
       return sequence_id
 
-    def create_journal_document(self, cr, uid, letter_ids, document_type, journal, wizard, context=None):
+    def create_journal_document(self, cr, uid, letter_ids, document_type, journal_id, credit_notes, debit_notes, context=None):
+      print letter_ids, document_type, journal_id, credit_notes, debit_notes
       document_class_obj = self.pool['afip.document_class']
       document_class_ids = document_class_obj.search(cr, uid, [('document_letter_id', 'in', letter_ids),('document_type', '=', document_type)], context=context)
       journal_document_obj = self.pool['account.journal.afip_document_class']
+      journal = self.pool['account.journal'].browse(cr, uid, journal_id, context=context)
       sequence = 10
-      print 'document_class_ids', document_class_ids
       for document_class in document_class_obj.browse(cr, uid, document_class_ids, context=context):
         sequence_id = False
         if journal.type == 'sale':
-          if document_type == 'invoice' or document_type == 'debit_note' and wizard.debit_notes == 'own_sequence':
+          if document_type == 'invoice' or document_type == 'debit_note' and debit_notes == 'own_sequence':
             sequence_id = self.create_sequence(cr, uid, document_class.name, journal, context)
-          elif document_type == 'debit_note' and wizard.debit_notes == 'same_sequence':
+          elif document_type == 'debit_note' and debit_notes == 'same_sequence':
             domain = [('afip_document_class_id.document_letter_id','=',document_class.document_letter_id.id),
               ('journal_id.point_of_sale','=',journal.point_of_sale)]
-            journal_docuent_ids = journal_document_obj.search(cr, uid, domain, context=context)
+            journal_docuent_ids = journal_document_obj.search(cr, uid, [('afip_document_class_id.document_letter_id','=',document_class.document_letter_id.id),
+              ('journal_id.point_of_sale','=',False)], context=context)
+            if not journal_docuent_ids:
+              journal_docuent_ids = journal_document_obj.search(cr, uid, domain, context=context)
             if journal_docuent_ids:
               sequence_id = journal_document_obj.browse(cr, uid, journal_docuent_ids[0], context=context).sequence_id.id
         elif journal.type == 'sale_refund':
-          if wizard.credit_notes == 'own_sequence':
+          if credit_notes == 'own_sequence':
             sequence_id = self.create_sequence(cr, uid, document_class.name, journal, context)
-          elif wizard.credit_notes == 'same_sequence':
+          elif credit_notes == 'same_sequence':
             domain = [('afip_document_class_id.document_letter_id','=',document_class.document_letter_id.id),
               ('journal_id.point_of_sale','=',journal.point_of_sale)]
             journal_docuent_ids = journal_document_obj.search(cr, uid, domain, context=context)

8acd0e60f98efd8d16ef217c34b6652ae491b72e
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-08-02 18:32:01 -0300

    ADD invoice report

---
+++ l10n_ar_report_invoice/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Ingenieria ADHOC - ADHOC SA
+#    https://launchpad.net/~ingenieria-adhoc
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Argentinian Like Invoice Report',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Argentina Aeroo Invoice Report
+==============================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'report_extended_account',
+        'l10n_ar_invoice',
+    ],
+    'data': [
+        'data/invoice_report.xml',
+        'views/report_invoice.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

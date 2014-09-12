25331c5bb117f0098a99f8df6313b099f2b5c740
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-09-12 11:33:00 -0300

    IMP aeroo reports

---
+++ l10n_ar_aeroo_receipt/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Argentinian Like Receipt Aeroo Report',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Argentinian Like Receipt Aeroo Report
+=====================================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'report_extended_voucher_receipt',
+        'account_check',
+        'l10n_ar_aeroo_base',
+    ],
+    'data': [
+        'receipt_report.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

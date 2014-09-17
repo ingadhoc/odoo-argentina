dd348f81d9ecfbb130c6f9347a11a92c86c6b6e7
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-09-17 20:08:29 -0300

    IMP reportes

---
+++ l10n_ar_aeroo_stock/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Argentinian Like Stock Picking Aeroo Report',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Argentinian Like Stock Picking Aeroo Report
+===========================================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'report_extended_stock',
+        'l10n_ar_aeroo_base',
+    ],
+    'data': [
+        'report.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

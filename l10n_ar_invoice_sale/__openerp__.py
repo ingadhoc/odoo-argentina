d2261dc691a235d3146d314192d7cc7d3b481348
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-23 10:31:15 -0300

    IMP description

--- l10n_ar_invoice_sale/__openerp__.py
+++ l10n_ar_invoice_sale/__openerp__.py
@@ -9,6 +9,9 @@
 Argentinian Sale Total Fields
 =============================
 Add fields in sale orders so that you can print sale orders with vay included or not depending on VAT responsabilities
+NOTAS PARA MEJORAR Y TRADUCIR:
+* Para usar esta funcionalidad tnees que ir a la compania, pestañá config, grupo "sale" y setear si querés y el valor por defecto que querés que tome si no hay match (para el caso de facu sería "no_discriminated_default" de (manera predeterminada no se discrimnan los impuestos, es decir que si n tiene seteado nada el partner entonces no se discrimina)
+* En los presupuestos, igualmente, si lo quiere cambiar ,en la segunda pestaña hay un campo "vat discriminated", abajo de posición fiscal, donde se 
     """,
     'author':  'Ingenieria ADHOC',
     'website': 'www.ingadhoc.com',

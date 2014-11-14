af918bd7247b300904796536ae541d2d8343b614
nicomacr <nmr@ingadhoc.com>
2014-11-14 12:48:55 -0300

    FIX requier and defaul company select

--- l10n_ar_account_followup/wizard/account_company_select.py
+++ l10n_ar_account_followup/wizard/account_company_select.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import models, fields, _
 
 
-class select_company_wizard(osv.osv_memory):
+class select_company_wizard(models.TransientModel):
     _name = 'select.company.wizard'
-    _columns = {
-        'company_id':fields.many2one('res.company', string="Company"),
-        }
+    
+    company_id = fields.Many2one('res.company', string="Company", default=lambda self: self.env.user.company_id.id)
+        
 
     
     def confirm_company(self, cr, uid, ids, context=None):
@@ -22,4 +21,6 @@ class select_company_wizard(osv.osv_memory):
                 return partner_obj.do_button_print(cr, uid, [partner_id], company_id=company_id, context=None)
             elif context['mode'] == 'mail':
                 return partner_obj.do_partner_mail(cr, uid, [partner_id], context=context)
+
+    
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

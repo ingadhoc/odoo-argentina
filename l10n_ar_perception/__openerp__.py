88aaca03d0579acc09c60bf3e4cd7f6ba5580c3c
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-11-18 17:36:05 -0300

    INIT preception module

---
+++ l10n_ar_perception/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Argentinian Invoice Percepcitons Management',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+Argentinian Invoice Percepcitons Management
+===========================================
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'account_invoice_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

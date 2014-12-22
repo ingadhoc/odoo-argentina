15d9d0e15d6ed8fd096bc7f65d0a292e333acae0
Nicolas Mac Rouillon <nmr@ingadhoc.com>
2014-12-22 11:46:13 -0300

    IMP add CBU in bank account

---
+++ l10n_ar_bank_cbu/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+import l10n_ar_bank_cbu
---
+++ l10n_ar_bank_cbu/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'CBU Banks ',
+    'version': '1.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'summary': '',
+    'description': """
+CBU Banks
+=========
+    """,
+    'author':  'Ingenieria ADHOC',
+    'website': 'www.ingadhoc.com',
+    'images': [
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'l10n_ar_bank.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

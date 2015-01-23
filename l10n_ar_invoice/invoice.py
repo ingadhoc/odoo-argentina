ee6ef5f6ae40112522343f7f9de0ea68b80b1572
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-01-23 12:16:01 -0300

    FIX printed values calculation and migrate to new api

--- l10n_ar_invoice/invoice.py
+++ l10n_ar_invoice/invoice.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from openerp import osv, models, fields, api, _
-from openerp.osv import fields as old_fields
 from openerp.exceptions import except_orm, Warning
 import openerp.addons.decimal_precision as dp
 
@@ -8,168 +7,152 @@ import openerp.addons.decimal_precision as dp
 class account_invoice_line(models.Model):
 
     """
-    En argentina como no se diferencian los impuestos en las facturas, excepto el IVA,
-    agrego campos que ignoran el iva solamenta a la hora de imprimir los valores.
+    En argentina como no se diferencian los impuestos en las facturas, excepto
+    el IVA, agrego campos que ignoran el iva solamenta a la hora de imprimir
+    los valores.
     """
 
     _inherit = "account.invoice.line"
 
-    def _printed_prices(self, cr, uid, ids, name, args, context=None):
-        res = {}
-        tax_obj = self.pool['account.tax']
-        cur_obj = self.pool.get('res.currency')
+    @api.one
+    def _printed_prices(self):
+        taxes = self.env['account.tax']
 
-        for line in self.browse(cr, uid, ids, context=context):
-            _round = (lambda x: cur_obj.round(
-                cr, uid, line.invoice_id.currency_id, x)) if line.invoice_id else (lambda x: x)
-            quantity = line.quantity
-            discount = line.discount
-            printed_price_unit = line.price_unit
-            printed_price_net = line.price_unit * \
-                (1 - (discount or 0.0) / 100.0)
-            printed_price_subtotal = printed_price_net * quantity
+        price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
 
-            not_vat_taxes = [
-                x for x in line.invoice_line_tax_id if x.tax_code_id.parent_id.name != 'IVA']
-            taxes = tax_obj.compute_all(cr, uid,
-                                        not_vat_taxes, printed_price_net, 1,
-                                        product=line.product_id,
-                                        partner=line.invoice_id.partner_id)
-            other_taxes_amount = _round(
-                taxes['total_included']) - _round(taxes['total'])
+        price_unit_without_tax = self.invoice_line_tax_id.compute_all(
+            self.price_unit, self.quantity, product=self.product_id,
+            partner=self.invoice_id.partner_id)
 
-            # TODO: tal vez mejorar esto de que se buscan los iva por el que tiene padre llamado "IVA"
-            # Antes habiamos agregado un vampo vat_tax en los code pero el tema
-            # es que tambien hay que agregarlo en el template de los tax code y
-            # en los planes de cuenta, resulta medio engorroso
-            vat_taxes = [
-                x for x in line.invoice_line_tax_id if x.tax_code_id.parent_id.name == 'IVA']
-            taxes = tax_obj.compute_all(cr, uid,
-                                        vat_taxes, printed_price_net, 1,
-                                        product=line.product_id,
-                                        partner=line.invoice_id.partner_id)
-            vat_amount = _round(
-                taxes['total_included']) - _round(taxes['total'])
+        # For document that not discriminate we include the prices
+        if self.invoice_id.vat_discriminated:
+            printed_price_unit = price_unit_without_tax['total']
+            printed_price_net = price_unit_without_tax['total'] * (
+                1 - (self.discount or 0.0) / 100.0)
+            printed_price_subtotal = printed_price_net * self.quantity
+        else:
+            printed_price_unit = price_unit_without_tax['total_included']
+            # printed_price_unit = all_taxes['total_included'] * (
+            #     1 + (self.discount or 0.0) / 100.0)
+            printed_price_net = price_unit_without_tax['total_included'] * (
+                1 - (self.discount or 0.0) / 100.0)
+            printed_price_subtotal = printed_price_net * self.quantity
 
-            exempt_amount = 0.0
-            if not vat_taxes:
-                exempt_amount = _round(taxes['total_included'])
+        self.printed_price_unit = printed_price_unit
+        self.printed_price_net = printed_price_net
+        self.printed_price_subtotal = printed_price_subtotal
 
-            # For document that not discriminate we include the prices
-            if not line.invoice_id.vat_discriminated:
-                printed_price_unit = _round(
-                    taxes['total_included'] * (1 + (discount or 0.0) / 100.0))
-                printed_price_net = _round(taxes['total_included'])
-                printed_price_subtotal = _round(
-                    taxes['total_included'] * quantity)
+        # Not VAT taxes
+        not_vat_taxes = self.invoice_line_tax_id.filtered(
+            lambda r: r.tax_code_id.parent_id.name != 'IVA').compute_all(
+            price, 1,
+            product=self.product_id,
+            partner=self.invoice_id.partner_id)
+        not_vat_taxes_amount = not_vat_taxes[
+            'total_included'] - not_vat_taxes['total']
 
-            res[line.id] = {
-                'printed_price_unit': printed_price_unit,
-                'printed_price_net': printed_price_net,
-                'printed_price_subtotal': printed_price_subtotal,
-                'vat_amount': vat_amount * quantity,
-                'other_taxes_amount': other_taxes_amount * quantity,
-                'exempt_amount': exempt_amount * quantity,
-            }
-        return res
+        # VAT taxes
+        vat_taxes = self.invoice_line_tax_id.filtered(
+            lambda r: r.tax_code_id.parent_id.name == 'IVA').compute_all(
+            price, 1,
+            product=self.product_id,
+            partner=self.invoice_id.partner_id)
+        vat_taxes_amount = vat_taxes['total_included'] - vat_taxes['total']
 
-    _columns = {
-        'printed_price_unit': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Unit Price', multi='printed',),
-        'printed_price_net': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Net Price', multi='printed'),
-        'printed_price_subtotal': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Subtotal', multi='printed'),
-        'vat_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Vat Amount', multi='printed'),
-        'other_taxes_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Other Taxes Amount', multi='printed'),
-        'exempt_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Exempt Amount', multi='printed'),
-    }
+        exempt_amount = 0.0
+        if not vat_taxes:
+            exempt_amount = taxes['total_included']
+
+        self.vat_amount = vat_taxes_amount * self.quantity
+        self.other_taxes_amount = not_vat_taxes_amount * self.quantity
+        self.exempt_amount = exempt_amount * self.quantity
+
+    printed_price_unit = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Unit Price')
+    printed_price_net = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Net Price')
+    printed_price_subtotal = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Subtotal')
+    vat_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Vat Amount')
+    other_taxes_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Other Taxes Amount')
+    exempt_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Exempt Amount')
 
 
 class account_invoice(models.Model):
     _inherit = "account.invoice"
 
-    def _printed_prices(self, cr, uid, ids, name, args, context=None):
-        res = {}
+    @api.one
+    def _printed_prices(self):
 
-        for invoice in self.browse(cr, uid, ids, context=context):
-            printed_amount_untaxed = invoice.amount_untaxed
-            printed_tax_ids = [x.id for x in invoice.tax_line]
+        vat_amount = sum([
+                x.tax_amount for x in self.tax_line if x.tax_code_id.parent_id.name == 'IVA'])
+        other_taxes_amount = sum(
+            line.other_taxes_amount for line in self.invoice_line)
+        exempt_amount = sum(
+            line.exempt_amount for line in self.invoice_line)
+        vat_tax_ids = [
+            x.id for x in self.tax_line if x.tax_code_id.parent_id.name == 'IVA']
 
-            # vat_amount = sum(
-            #     line.vat_amount for line in invoice.invoice_line)
-            # Por errores de redonde cambiamos la forma anterior por esta nueva
-            vat_amount = sum([
-                x.tax_amount for x in invoice.tax_line if x.tax_code_id.parent_id.name == 'IVA'])
+        if self.vat_discriminated:
+            printed_amount_untaxed = self.amount_untaxed
+            printed_tax_ids = [x.id for x in self.tax_line]
+        else:
+            printed_amount_untaxed = sum(
+                line.printed_price_subtotal for line in self.invoice_line)
+            printed_tax_ids = [
+                    x.id for x in self.tax_line if x.tax_code_id.parent_id.name != 'IVA']
 
-            other_taxes_amount = sum(
-                line.other_taxes_amount for line in invoice.invoice_line)
-            exempt_amount = sum(
-                line.exempt_amount for line in invoice.invoice_line)
-            vat_tax_ids = [
-                x.id for x in invoice.tax_line if x.tax_code_id.parent_id.name == 'IVA']
+        self.printed_amount_untaxed = printed_amount_untaxed
+        self.printed_tax_ids = printed_tax_ids
+        self.printed_amount_tax = self.amount_total - printed_amount_untaxed
+        self.vat_tax_ids = vat_tax_ids
+        self.vat_amount = vat_amount
+        self.other_taxes_amount = other_taxes_amount
+        self.exempt_amount = exempt_amount
 
-            if not invoice.vat_discriminated:
-                printed_amount_untaxed = sum(
-                    line.printed_price_subtotal for line in invoice.invoice_line)
-                printed_tax_ids = [
-                    x.id for x in invoice.tax_line if x.tax_code_id.parent_id.name != 'IVA']
-            res[invoice.id] = {
-                'printed_amount_untaxed': printed_amount_untaxed,
-                'printed_tax_ids': printed_tax_ids,
-                'printed_amount_tax': invoice.amount_total - printed_amount_untaxed,
-                'vat_tax_ids': vat_tax_ids,
-                'vat_amount': vat_amount,
-                'other_taxes_amount': other_taxes_amount,
-                'exempt_amount': exempt_amount,
-            }
-        return res
-
-    _columns = {
-        'printed_amount_tax': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Tax', multi='printed',),
-        'printed_amount_untaxed': old_fields.function(
-            _printed_prices,
-            type='float', digits_compute=dp.get_precision('Account'),
-            string='Subtotal', multi='printed',),
-        'printed_tax_ids': old_fields.function(
-            _printed_prices,
-            type='one2many', relation='account.invoice.tax', string='Tax',
-            multi='printed'),
-        'exempt_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Exempt Amount', multi='printed'),
-        'vat_tax_ids': old_fields.function(
-            _printed_prices,
-            type='one2many', relation='account.invoice.tax',
-            string='VAT Taxes', multi='printed'),
-        'vat_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Vat Amount', multi='printed'),
-        'other_taxes_amount': old_fields.function(
-            _printed_prices, type='float',
-            digits_compute=dp.get_precision('Account'),
-            string='Other Taxes Amount', multi='printed')
-    }
+    printed_amount_tax = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Tax')
+    printed_amount_untaxed = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Subtotal')
+    exempt_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Exempt Amount')
+    vat_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Vat Amount')
+    other_taxes_amount = fields.Float(
+        compute="_printed_prices",
+        digits_compute=dp.get_precision('Account'),
+        string='Other Taxes Amount')
+    printed_tax_ids = fields.One2many(
+        compute="_printed_prices",
+        comodel_name='account.invoice.tax',
+        string='Tax')
+    vat_tax_ids = fields.One2many(
+        compute="_printed_prices",
+        comodel_name='account.invoice.tax',
+        string='VAT Taxes')
 
     @api.multi
     def name_get(self):
@@ -246,7 +229,7 @@ class account_invoice(models.Model):
         'afip_document_class_id.document_letter_id',
         'afip_document_class_id.document_letter_id.vat_discriminated',
         'company_id',
-        'company_id.invoice_vat_discrimination_default',)
+        'company_id.invoice_vat_discrimination_default')
     def get_vat_discriminated(self):
         vat_discriminated = False
         if self.afip_document_class_id.document_letter_id.vat_discriminated or self.company_id.invoice_vat_discrimination_default == 'discriminate_default':

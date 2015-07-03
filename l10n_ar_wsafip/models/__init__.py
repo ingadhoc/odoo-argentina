1345fa78e45e6737df63b897f7887ca87290b447
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-03 02:41:08 -0300

    ADD loadcert_config to make module uninstallable

--- l10n_ar_wsafip/models/__init__.py
+++ l10n_ar_wsafip/models/__init__.py
@@ -3,5 +3,6 @@ import wsafip_certificate_alias
 import wsafip_certificate
 import wsafip_server
 import wsafip_connection
+import loadcert_config
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

aa1e78de9ce5e1bfd26c7b8b3943c1db080ee503
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-03 02:42:14 -0300

    ADD missing file

---
+++ l10n_ar_wsafip/models/loadcert_config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from openerp import models
+
+
+class l10n_ar_wsafip_loadcert_config(models.TransientModel):
+    _name = 'l10n_ar_wsafip.loadcert_config'
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

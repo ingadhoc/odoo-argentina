a76a9bdbe6424357d974b6f18bd3164b6038d240
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-03 02:44:26 -0300

    IMP

--- l10n_ar_wsafip/models/__init__.py
+++ l10n_ar_wsafip/models/__init__.py
@@ -3,6 +3,6 @@ import wsafip_certificate_alias
 import wsafip_certificate
 import wsafip_server
 import wsafip_connection
-import loadcert_config
+import missing_models_for_uninstall
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

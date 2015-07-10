85ff69a6bc1a80e25badb959d47ed0d9cef6c2bb
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-10 10:33:00 -0300

    IMP tax settlement

--- l10n_ar_account_tax_settlement/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-import account_journal
-import account_move_line
-import tax_settlement
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- l10n_ar_chart_generic_tax_settlement/__openerp__.py
+++ l10n_ar_chart_generic_tax_settlement/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Retenciones para Plan Contable General Argentino',
+    'name': 'Liquidacion de Impuestos para Plan Contable General Argentino',
     'author':  'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'version': '8.0.1',
@@ -9,25 +9,26 @@
     'summary': '',
     'license': 'AGPL-3',
     'description': """
-Retenciones para Plan Contable General Argentino
-================================================
-""",
+Liquidacion de Impuestos para Plan Contable General Argentino
+=============================================================
+TODO implementar cambios como los hace l10n_ar_chart_generic_withholding
+    """,
+    'images': [
+    ],
     'depends': [
         'l10n_ar_chart_generic',
-        'account_voucher_withholding',
+        'account_tax_settlement',
     ],
     'data': [
-        'data/account_chart_respinsc.xml',
+
     ],
     'demo': [
-        'demo/ri_withholding_demo.xml',
-        'demo/ri_voucher_demo.xml',
+        'demo/demo_data.xml',
     ],
     'test': [
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': False,
     'application': False,
 }
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

e8bb29e2da187a274fa27a3f1dbcf75bb97e32e0
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-16 08:39:23 -0300

    IMP demo invoices again

--- l10n_ar_invoice/__openerp__.py
+++ l10n_ar_invoice/__openerp__.py
@@ -95,8 +95,8 @@ Para actualizar el plan de cuentas de datos demo:
           'demo/fiscal_year_ri_demo.xml',
           'demo/account_chart_respinsc.xml',
           'demo/account_demo.xml',
-          # 'demo/ri_purchase_invoice_demo.xml',
-          # 'demo/ri_sale_invoice_demo.xml',
+          'demo/ri_purchase_invoice_demo.xml',
+          'demo/ri_sale_invoice_demo.xml',
     ],
     'test': [
     ],

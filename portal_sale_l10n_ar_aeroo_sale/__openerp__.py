059e632d184b015c335ab0456fbc5e3b20ac3231
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2015-07-23 14:24:47 -0300

    FIX module author

--- portal_sale_l10n_ar_aeroo_sale/__openerp__.py
+++ portal_sale_l10n_ar_aeroo_sale/__openerp__.py
@@ -31,8 +31,8 @@
 Argentinian Like Sale Order Integration With Portal Sale
 ========================================================
     """,
-    'author':  'Ingenieria ADHOC',
-    'website': 'www.ingadhoc.com',
+    'author':  'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
     'images': [
     ],
     'depends': [

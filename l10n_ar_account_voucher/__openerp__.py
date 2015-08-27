787cf63c1c4d3835e7dbb1fdea9fe494c6edddd5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-27 16:11:09 -0300

    DEL force_number from voucher view

--- l10n_ar_account_voucher/__openerp__.py
+++ l10n_ar_account_voucher/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Aregentinian Receipts and Payment Orders',
-    'version': '8.0.1.0.0',
+    'version': '8.0.1.1.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author':  'ADHOC SA',

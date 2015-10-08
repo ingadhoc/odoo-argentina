2e2b1a0756ebf799b218ff2d0d49ed04e1648ba7
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-10-08 16:50:48 -0300

    FIX invoice currency

--- l10n_ar_invoice/__openerp__.py
+++ l10n_ar_invoice/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Argentina - Facturación y documentos AFIP",
-    'version': '8.0.0.5.1',
+    'version': '8.0.0.5.2',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author':  'ADHOC SA',

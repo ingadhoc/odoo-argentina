d0405484ecf6b3b060918b0760deed20d0005f85
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-10-10 12:24:31 -0300

    IMP del init on l10 account voucher

--- l10n_ar_account_voucher/models/account_voucher_receiptbook.py
+++ l10n_ar_account_voucher/models/account_voucher_receiptbook.py
@@ -14,47 +14,6 @@ class account_voucher_receiptbook(models.Model):
     _description = 'Account Voucher Receiptbook'
     _order = 'sequence asc'
 
-    def init(self, cr):
-        """
-        Esto es para instalar si teniamos account_voucher_receipt y antes de
-        desinstalar este ultimo para no perder algunos datos
-        """
-        _logger.info(
-            "Update data ids to 'l10n_ar_account_voucher'")
-
-        data_ids = [
-            'sequence_customer_receipt_1',
-            'sequence_supplier_receipt_1',
-            'customer_receiptbook_1',
-            'supplier_receiptbook_1',
-            'customer_receiptbook_2',
-            'supplier_receiptbook_2',
-            ]
-
-        cr.execute(
-            """UPDATE ir_model_data set module='l10n_ar_account_voucher' where
-            module='account_voucher_receipt' and name in %s""" % (
-                str(tuple(data_ids))))
-
-        _logger.info(
-            "Updating fields external id for receiptbook "
-            "so you can uninstall this module'")
-        field_prefix = 'field_account_voucher_receiptbook_'
-        fields_sufixs = [
-            'sequence', 'name', 'type', 'sequence_type', 'sequence_id',
-            'company_id', 'manual_prefix', 'padding', 'active',
-            'document_class_id']
-        fields_with_prefix = []
-        for field in fields_sufixs:
-            fields_with_prefix.append("%s%s" % (field_prefix, field))
-
-        _logger.info("Moving fields of account_voucher_receipt' if installed")
-
-        cr.execute(
-            """UPDATE ir_model_data set module='l10n_ar_account_voucher' where
-            module='account_voucher_receipt' and name in %s""" % (
-                str(tuple(fields_with_prefix))))
-
     sequence = fields.Integer(
         'Sequence',
         help="Used to order the receiptbooks",

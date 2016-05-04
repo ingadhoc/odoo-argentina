6e8b96e95f13cd3b43e3ea64ff73d3ad53921c5d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-05-04 18:06:58 -0300

    FIX vat ledger

--- l10n_ar_account_vat_ledger/__openerp__.py
+++ l10n_ar_account_vat_ledger/__openerp__.py
@@ -20,10 +20,10 @@
 ##############################################################################
 {
     "name": "Argentinian VAT Ledger Management",
-    'version': '8.0.1.0.4',
+    'version': '8.0.1.1.4',
     'category': 'Localization/Argentina',
     'sequence': 14,
-    'author':  'ADHOC SA',
+    'author': 'ADHOC SA',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',
     'summary': '',

86df7fa4f05af3e383228a39c341c9da1c76117d
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-05-20 16:54:37 -0300

    ADD l10n_ar_afipws_fe_cancel/

---
+++ l10n_ar_afipws_fe_cancel/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Integration between electronic invoice and account cancel",
+    'version': '8.0.0.0.0',
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """
+Integration between electronic invoice and account cancel
+=========================================================
+""",
+    'depends': [
+        'l10n_ar_afipws_fe',
+        'account_cancel',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/invoice_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

335523283e8be3861c325f84c38476801354442a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-08-12 15:06:24 -0300

    ADD l10n_ar_partner from odoo-argentina/partner

---
+++ l10n_ar_partner/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'author': "Moldeo Interactive,ADHOC SA",
+    'category': 'Localization/Argentina',
+    'demo_xml': [],
+    'depends': ['partner_documents'],
+    'init_xml': [],
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': 'Argentina - Datos de documentos y títulos de personerías',
+    'data': [
+        'data/res_partner_title.xml',
+        'data/res_partner_document_type.xml',
+        'demo/partner_demo.xml',
+    ],
+    'version': '9.0.0.0.0',
+}

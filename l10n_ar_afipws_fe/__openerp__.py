093c010be63725d8fcc1c94dad87849afea1d42e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-06 12:01:35 -0300

    IMP make fe not auto install

--- l10n_ar_afipws_fe/__openerp__.py
+++ l10n_ar_afipws_fe/__openerp__.py
@@ -58,7 +58,7 @@ algun lugar
     'test': [
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': False,
     'application': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

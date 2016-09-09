6777e35fd698c820a008216502f2b9c5e7416a2c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-09-09 12:31:16 -0300

    IMP supplier number required again

--- l10n_ar_invoice/__openerp__.py
+++ l10n_ar_invoice/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Argentina - Facturación y documentos AFIP",
-    'version': '8.0.0.22.0',
+    'version': '8.0.0.23.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

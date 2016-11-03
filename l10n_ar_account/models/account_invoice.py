b6484070795ba3c79cb83cb053bf4c310aecc12f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-03 17:48:10 -0300

    FIX codes

--- l10n_ar_account/models/account_invoice.py
+++ l10n_ar_account/models/account_invoice.py
@@ -184,12 +184,12 @@ class AccountInvoice(models.Model):
         # decidimos obtener esto solamente para comprobantes con doc number
         str_number = self.document_number or False
         if str_number:
-            if self.document_type_id.code in [33, 99, 331, 332]:
+            if self.document_type_id.code in ['33', '99', '331', '332']:
                 point_of_sale = '0'
                 # leave only numbers and convert to integer
                 invoice_number = str_number
             # despachos de importacion
-            elif self.document_type_id.code == 66:
+            elif self.document_type_id.code == '66':
                 point_of_sale = '0'
                 invoice_number = '0'
             elif "-" in str_number:
@@ -233,7 +233,7 @@ class AccountInvoice(models.Model):
                 afip_concept = '2'
             if product_types.issubset(consumible):
                 afip_concept = '1'
-            if self.document_type_id.code in [19, 20, 21]:
+            if self.document_type_id.code in ['19', '20', '21']:
                 # TODO verificar esto, como par expo no existe 3 y existe 4
                 # (otros), considermaos que un mixto seria el otros
                 if afip_concept == '3':

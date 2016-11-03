2d17dc1910be29db660666d77e4f4c4044274c5b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-03 18:44:35 -0300

    FIX

--- l10n_ar_account/wizard/account_invoice_refund.py
+++ l10n_ar_account/wizard/account_invoice_refund.py
@@ -13,14 +13,15 @@ class AccountInvoiceRefund(models.TransientModel):
     @api.multi
     def compute_refund(self, mode='refund'):
         res = super(AccountInvoiceRefund, self).compute_refund(mode=mode)
-        domain = res.get('domain', [])
-        refund_invoices = self.env['account.invoice'].search(domain)
-        # invoice = self.env['account.invoice'].browse(invoice_ids)
-        invoice = self.invoice_id
-        refund_invoices.write({
-            # TODO this origin should be set on account_document module
-            'origin': invoice.document_number or invoice.number,
-            'afip_service_start': invoice.afip_service_start,
-            'afip_service_end': invoice.afip_service_end,
-        })
+        if res:
+            domain = res.get('domain', [])
+            refund_invoices = self.env['account.invoice'].search(domain)
+            # invoice = self.env['account.invoice'].browse(invoice_ids)
+            invoice = self.invoice_id
+            refund_invoices.write({
+                # TODO this origin should be set on account_document module
+                'origin': invoice.document_number or invoice.number,
+                'afip_service_start': invoice.afip_service_start,
+                'afip_service_end': invoice.afip_service_end,
+            })
         return res

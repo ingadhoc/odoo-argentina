33d8c052261a08cba076c6a6cae7aad21c350847
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-04 16:21:59 -0300

    MIG currency update

--- l10n_ar_currency_update/models/currency_rate_update.py
+++ l10n_ar_currency_update/models/currency_rate_update.py
@@ -4,26 +4,18 @@
 # directory
 ##############################################################################
 from openerp import fields, models, api, _
-from openerp.exceptions import Warning
+from openerp.exceptions import UserError
 from datetime import datetime, time
 from dateutil.relativedelta import relativedelta
 import logging
-# import openerp.addons.decimal_precision as dp
-from openerp.addons.currency_rate_update.model.currency_rate_update import supported_currecies
 
 _logger = logging.getLogger(__name__)
 
 _intervalTypes = {
     'days': lambda interval: relativedelta(days=interval),
-    'weeks': lambda interval: relativedelta(days=7*interval),
+    'weeks': lambda interval: relativedelta(days=7 * interval),
     'months': lambda interval: relativedelta(months=interval),
 }
-
-# TODO completar con todas las monedas soportadas
-AFIP_WS_supported_currency_array = [
-    'USD', 'EUR', 'AUD', 'CAD', 'GBP', 'JPY', 'MXN', 'UYU', 'VEF']
-
-supported_currecies['afip_ws'] = AFIP_WS_supported_currency_array
 
 
 class Currency_rate_update_service(models.Model):
@@ -31,19 +23,14 @@ class Currency_rate_update_service(models.Model):
     have to be updated"""
     _inherit = "currency.rate.update.service"
 
-    service = fields.Selection(
-        selection_add=([('afip_ws', 'AFIP WS')])
-        )
     rate_perc = fields.Float(
         'Rate Perc.',
         digits=(16, 4),
-        # digits=dp.get_precision('Product Price'),
-        )
+    )
     rate_surcharge = fields.Float(
         'Rate Surcharge',
         digits=(16, 4),
-        # digits=dp.get_precision('Product Price'),
-        )
+    )
 
     @api.one
     def refres_currency_with_afip_ws(self):
@@ -55,28 +42,20 @@ class Currency_rate_update_service(models.Model):
             'Starting to refresh currencies with service %s (company: %s)',
             self.service, self.company_id.name)
         # factory = Currency_getter_factory()
-        curr_obj = self.env['res.currency']
         rate_obj = self.env['res.currency.rate']
         company = self.company_id
         # The multi company currency can be set or no so we handle
         # The two case
         if company.auto_currency_up:
-            main_currency = curr_obj.search(
-                [('base', '=', True), ('company_id', '=', company.id)],
-                limit=1)
+            main_currency = self.company_id.currency_id
             if not main_currency:
-                # If we can not find a base currency for this company
-                # we look for one with no company set
-                main_currency = curr_obj.search(
-                    [('base', '=', True), ('company_id', '=', False)],
-                    limit=1)
-            if not main_currency:
-                raise Warning(_('There is no base currency set!'))
+                raise UserError(_(
+                    'There is no main currency defined!'))
             if main_currency.rate != 1:
-                raise Warning(_('Base currency rate should be 1.00!'))
+                raise UserError(_('Base currency rate should be 1.00!'))
             # TODO implementar poder usar otras monedas como base
             if main_currency.name != 'ARS':
-                raise Warning(_('For AFIP WS base currency must be ARS!'))
+                raise UserError(_('For AFIP WS base currency must be ARS!'))
             note = self.note or ''
             try:
                 rate_name = \
@@ -87,9 +66,9 @@ class Currency_rate_update_service(models.Model):
                         continue
                     rate, msg = curr.get_pyafipws_currency_rate(
                         company=self.company_id,
-                        )
+                    )
                     if rate:
-                        rate = rate * (1.0+(self.rate_perc or 0.0))
+                        rate = rate * (1.0 + (self.rate_perc or 0.0))
                         rate += self.rate_surcharge or 0.0
                         rate = 1.0 / rate
                         vals = {

f476d98e0963d7d145af0f46ff01cbecbaefcce4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-04 11:44:37 -0300

    MIG vat ledg

--- l10n_ar_account_vat_ledger/__openerp__.py
+++ l10n_ar_account_vat_ledger/__openerp__.py
@@ -9,16 +9,16 @@
     'summary': '',
     "depends": [
         # TODO we should try to get this report with another tool, not aeroo
-        # "report_aeroo",
+        "report_aeroo",
         "l10n_ar_account"
     ],
     'external_dependencies': {
     },
     "data": [
         'account_vat_report_view.xml',
-        'report/account_vat_ledger_report.xml',
-        'security/security.xml',
-        'security/ir.model.access.csv',
+        # 'report/account_vat_ledger_report.xml',
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
     ],
     'demo': [
     ],
@@ -26,7 +26,7 @@
     ],
     'images': [
     ],
-    "installable": False,
+    "installable": True,
     'auto_install': True,
     'application': False,
 }

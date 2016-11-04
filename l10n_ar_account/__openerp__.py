9a7fd0eeccbec11ca1d44cc411d89879fa276672
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-04 10:42:53 -0300

    FIX commit

--- l10n_ar_account/__openerp__.py
+++ l10n_ar_account/__openerp__.py
@@ -12,10 +12,10 @@
         'l10n_ar_partner',
         # para guardar el link entre facturas y NC
         # el modulo tiene errores en los test (probado solo con odoo y tmb)
-        # 'account_invoice_refund_link',
+        'account_invoice_refund_link',
         # no es una dependencia en si (salvo para datos demo) pero si es
         # necesario por como implemenamos la localizacion
-        'account_invoice_tax_wizard',
+        # 'account_invoice_tax_wizard',
     ],
     'external_dependencies': {
     },

51530003f974ed00005a6f1daa4b020e6143e546
Juan José Scarafía <scarafia.juanjose@gmail.com>
2016-11-04 19:15:59 +0200

    9.0 mig bank (#149)
    * IMP menu
    
    * [9.0][MIG] bank
    
    * IMP imports

--- l10n_ar_bank/l10n_ar_bank.py
+++ l10n_ar_bank/l10n_ar_bank.py
@@ -1,38 +1,41 @@
 # -*- coding: utf-8 -*-
-from openerp import fields, models, api
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from openerp import fields, models, api, _
+from openerp.exceptions import UserError
 
 
-class Bank(models.Model):
-    _inherit = 'res.bank'
-
-    bcra_code = fields.Char('BCRA Code',
-                            size=8,
-                            help="Code assigned by BCRA")
-    vat = fields.Char('VAT',
-                      size=32,
-                      help="Value Added Tax number.")
-
-
-class partner_bank(models.Model):
-    _name = 'res.partner.bank'
+class ResPartnerBank(models.Model):
     _inherit = 'res.partner.bank'
+
+    cbu = fields.Char(
+        'CBU',
+        help="Código Bancario Único Argentino")
+
+    @api.constrains('cbu')
+    def check_cbu(self):
+        for rec in self:
+            if rec.cbu and not rec.is_valid_cbu():
+                raise UserError(_('El CBU "%s" no es válido') % rec.cbu)
 
     @api.multi
     def is_valid_cbu(self):
         self.ensure_one()
 
-        cbu = self.acc_number
+        cbu = self.cbu
 
         if type(cbu) == int:
             cbu = "%022d" % cbu
         cbu = cbu.strip()
         if len(cbu) != 22:
             return False
-        s1 = sum(int(a)*b for a, b in zip(cbu[0:7], (7, 1, 3, 9, 7, 1, 3)))
+        s1 = sum(int(a) * b for a, b in zip(cbu[0:7], (7, 1, 3, 9, 7, 1, 3)))
         d1 = (10 - s1) % 10
         if d1 != int(cbu[7]):
             return False
-        s2 = sum(int(a)*b
+        s2 = sum(int(a) * b
                  for a, b in zip(cbu[8:-1],
                                  (3, 9, 7, 1, 3, 9, 7, 1, 3, 9, 7, 1, 3)))
         d2 = (10 - s2) % 10
--- l10n_ar_bank/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-import wiz_l10n_ar_bank

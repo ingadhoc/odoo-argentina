9313dc65addad829ce0a080a67778a5e63356c65
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-07 10:03:55 -0300

    FIX lint

--- l10n_ar_aeroo_base/parser.py
+++ l10n_ar_aeroo_base/parser.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-import conversor
+from . import conversor
 from openerp.report.report_sxw import rml_parse
 import sys
 reload(sys)

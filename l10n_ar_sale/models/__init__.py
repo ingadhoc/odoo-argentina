e0f2f2571cf48d2dd9d44730a7e5c2bc5bad50a1
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-11-07 15:18:55 -0300

    DEL l10n_ar_sale

--- l10n_ar_sale/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-from . import res_company
-from . import sale_order
-from . import sale_order_line

d0e8e05a2c3ac6cd2dc4703f630498cf73606068
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-01 17:30:26 -0300

    IMP module install bypass

--- l10n_ar_base/__init__.py
+++ l10n_ar_base/__init__.py
@@ -3,4 +3,28 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from . import ir_module
+from openerp import SUPERUSER_ID
+
+from openerp.addons import account
+old_auto_install_l10n = account._auto_install_l10n
+
+
+def ar_auto_install_l10n(cr, registry):
+    """
+    overwrite of this function to install our localization module
+    """
+    country_code = registry['res.users'].browse(
+        cr, SUPERUSER_ID, SUPERUSER_ID, {}).company_id.country_id.code
+    if country_code:
+        if country_code == 'AR':
+            module_ids = registry['ir.module.module'].search(
+                cr, SUPERUSER_ID, [
+                    ('name', '=', 'l10n_ar_chart'),
+                    ('state', '=', 'uninstalled')])
+            registry['ir.module.module'].button_install(
+                cr, SUPERUSER_ID, module_ids, {})
+        else:
+            return old_auto_install_l10n
+
+
+account._auto_install_l10n = ar_auto_install_l10n

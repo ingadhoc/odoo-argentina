adaf5423b1067b330b9178f6d0dadcae31d73134
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-01 11:12:36 -0300

    IMP clean code

--- l10n_ar_account_withholding/models/account_voucher.py
+++ l10n_ar_account_withholding/models/account_voucher.py
@@ -75,21 +75,3 @@ class AccountVoucher(models.Model):
                 not voucher.regimen_ganancias_id):
             voucher.retencion_ganancias = 'no_aplica'
         return voucher
-
-    @api.multi
-    def compute_withholdings(self):
-        for voucher in self:
-            self.env['account.tax.withholding'].search([
-                ('type_tax_use', 'in', [self.type, 'all']),
-                ('company_id', '=', self.company_id.id),
-            ]).create_voucher_withholdings(voucher)
-
-    @api.multi
-    def action_confirm(self):
-        res = super(AccountVoucher, self).action_confirm()
-        self.search([
-            ('type', '=', 'payment'),
-            ('journal_id.automatic_withholdings', '=', True),
-            ('id', 'in', self.ids),
-        ]).compute_withholdings()
-        return res

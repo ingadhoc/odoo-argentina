3d799e3f4aa8ebfec99f5815fa33099ad3f74b78
Salvatore Josué Trimarchi Pinto <trimarchi@bacgroup.net>
2017-01-16 10:44:57 -0600

    [IMP] Migrate ar sale module

--- l10n_ar_sale/report/invoice_report.py
+++ l10n_ar_sale/report/invoice_report.py
@@ -9,22 +9,22 @@ from openerp import models, fields
 class account_invoice_report(models.Model):
     _inherit = 'account.invoice.report'
 
-    afip_document_class_id = fields.Many2one(
-        'afip.document_class',
+    document_type_id = fields.Many2one(
+        'account.document.type',
         string='Document Type',
         )
 
     _depends = {
-        'account.invoice': ['afip_document_class_id'],
+        'account.invoice': ['document_type_id'],
     }
 
     def _select(self):
         return super(
-            account_invoice_report, self)._select() + ", sub.afip_document_class_id as afip_document_class_id"
+            account_invoice_report, self)._select() + ", sub.document_type_id as document_type_id"
 
     def _sub_select(self):
         return super(
-            account_invoice_report, self)._sub_select() + ", ai.afip_document_class_id as afip_document_class_id"
+            account_invoice_report, self)._sub_select() + ", ai.document_type_id as document_type_id"
 
     def _group_by(self):
-        return super(account_invoice_report, self)._group_by() + ", ai.afip_document_class_id"
+        return super(account_invoice_report, self)._group_by() + ", ai.document_type_id"

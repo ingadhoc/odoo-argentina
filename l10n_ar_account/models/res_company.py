fdec18c053d69c609f901ce977641f98c09d53ca
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-26 16:54:38 -0300

    IMP add jurisdiction

--- l10n_ar_account/models/res_company.py
+++ l10n_ar_account/models/res_company.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, api
 from openerp.addons.account_document.models.res_company import ResCompany
 
 localizations = ResCompany._localization_selection
@@ -22,12 +22,20 @@ class ResCompany(models.Model):
         related='partner_id.gross_income_type',
         string='Gross Income'
     )
+    gross_income_jurisdiction_ids = fields.Many2many(
+        related='partner_id.gross_income_jurisdiction_ids',
+    )
     start_date = fields.Date(
         related='partner_id.start_date',
     )
     afip_responsability_type_id = fields.Many2one(
         related='partner_id.afip_responsability_type_id',
     )
+
+    @api.onchange('localization')
+    def change_localization(self):
+        if self.localization == 'argentina' and not self.country_id:
+            self.country_id = self.env.ref('base.ar')
     # TODO ver si lo movemos a account_document
     # journal_ids = fields.One2many(
     #     'account.journal',

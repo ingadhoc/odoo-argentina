78d14eeb4c79c5db5d28ac1c3fb9d317b944b1f9
Juan José Scarafía <jjs@adhoc.com.ar>
2017-03-11 17:40:58 -0300

    DEL tax hack constraint (#182)
    * DEL tax hack constraint
    
    * FIX demo data

--- l10n_ar_account/models/account_invoice.py
+++ l10n_ar_account/models/account_invoice.py
@@ -495,16 +495,20 @@ class AccountInvoice(models.Model):
                     "* Invoice id %i" % (afip_exempt_codes, invoice.id))
                 )
 
-    # TODO check if we can remove this. If we import or get demo data
+    # TODO sacamos esto porque no era muy lindo y daba algunos errores con
+    # el account_fix, hicimos que los datos demo hagan el compute tax
+    # habria que ver una mejor forma de hacerlo para que tambien ande bien si
+    # se importa desde interfaz
+    # If we import or get demo data
     # tax_id is not loaded on tax lines, we couldn't find the error
     # so we add this to fix it
-    @api.one
-    @api.constrains('invoice_line_ids')
-    def update_taxes_fix(self):
-        context = dict(self._context)
-        if context.get('constraint_update_taxes'):
-            return True
-        self.with_context(constraint_update_taxes=True).compute_taxes()
+    # @api.one
+    # @api.constrains('invoice_line_ids')
+    # def update_taxes_fix(self):
+    #     context = dict(self._context)
+    #     if context.get('constraint_update_taxes'):
+    #         return True
+    #     self.with_context(constraint_update_taxes=True).compute_taxes()
 
     # we add fiscal position with fp method instead of directly from partner
     # TODO. this should go in a PR to ODOO

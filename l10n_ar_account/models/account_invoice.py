f81f3808de1be4b47c2bb7a84d02110a6318ea0b
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-15 10:40:45 -0600

    VITTAR-77 [DEBUG] Debit Note Debug

--- l10n_ar_account/models/account_invoice.py
+++ l10n_ar_account/models/account_invoice.py
@@ -313,7 +313,8 @@ class AccountInvoice(models.Model):
                         domain + [
                             ('document_type_id.internal_type',
                                 '=', internal_type)], limit=1)
-                    _logger.critical("The name "+str(internal_type))
+                    _logger.critical("The name "+str(journal_document_type.name))
+                else:
                 # For domain, we search all documents
                 journal_document_types = journal_document_types.search(domain)
 

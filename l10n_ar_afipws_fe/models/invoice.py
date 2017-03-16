b2abfa2081dbe88c37c40de146808af3265fa5af
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-16 15:43:56 -0600

    VITTAR-115 [FIX] PYi25 import Error

--- l10n_ar_afipws_fe/models/invoice.py
+++ l10n_ar_afipws_fe/models/invoice.py
@@ -3,9 +3,9 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from .pyi25 import PyI25
-from openerp import fields, models, api, _
-from openerp.exceptions import UserError
+from pyafipws.pyi25 import PyI25
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 from cStringIO import StringIO as StringIO
 import logging
 import sys

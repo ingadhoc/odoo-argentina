f23cd68c531b06a7b5053d51689995719e31f768
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-03-20 20:32:05 -0300

    IMP add option to make id numbers unique

--- l10n_ar_partner/models/res_partner_id_number.py
+++ l10n_ar_partner/models/res_partner_id_number.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api, _
+from openerp.exceptions import UserError
+from openerp.tools.safe_eval import safe_eval
 
 
 class ResPartnerIdNumber(models.Model):
@@ -10,3 +12,17 @@ class ResPartnerIdNumber(models.Model):
         default=10,
         required=True,
     )
+
+    @api.multi
+    @api.constrains('name', 'category_id')
+    def check(self):
+        if not safe_eval(self.env['ir.config_parameter'].get_param(
+                "l10n_ar_partner.unique_id_numbers", 'False')):
+            return True
+        for rec in self:
+            if rec.search([
+                    ('name', '=', rec.name),
+                    ('category_id', '=', rec.category_id.id),
+                    ('id', '!=', rec.id)]):
+                raise UserError(_(
+                    'Id Number must be unique per id category'))
--- l10n_ar_partner/sale_config.py
+++ l10n_ar_partner/sale_config.py
@@ -3,7 +3,8 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import fields, models
+from openerp import fields, models, api
+from openerp.tools.safe_eval import safe_eval
 
 
 class SaleConfiguration(models.TransientModel):
@@ -15,3 +16,22 @@ class SaleConfiguration(models.TransientModel):
         "NUmbers' will be added on partner form view",
         implied_group='l10n_ar_partner.group_multiple_id_numbers',
     )
+    unique_id_numbers = fields.Boolean(
+        "Restrict Id Numbers to be Unique",
+        help="If you set it True, then we will check that partner Id Numbers "
+        "(for eg. cuit, dni, etc) are unique",
+    )
+
+    @api.multi
+    def get_default_unique_id_numbers(self):
+        unique_id_numbers = self.env['ir.config_parameter'].get_param(
+            "l10n_ar_partner.unique_id_numbers", 'False')
+        return {
+            'unique_id_numbers': safe_eval(unique_id_numbers),
+        }
+
+    @api.multi
+    def set_default_unique_id_numbers(self):
+        for record in self:
+            self.env['ir.config_parameter'].set_param(
+                "l10n_ar_partner.unique_id_numbers", record.unique_id_numbers)

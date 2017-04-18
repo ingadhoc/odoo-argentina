fbe3855312c2fdaca05fbc10d753013ffe6e0e40
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-04-18 17:16:48 -0300

    ADD sale team

--- l10n_ar_invoice/__openerp__.py
+++ l10n_ar_invoice/__openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Argentina - Facturación y documentos AFIP",
-    'version': '8.0.0.27.0',
+    'version': '8.0.0.28.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

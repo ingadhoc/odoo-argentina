95a01edd0c3a904b14acafd432566e0b847b95be
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-09-07 10:55:59 -0300

    REF add missing Odoo Community Association (OCA)

--- l10n_ar_bank/__manifest__.py
+++ l10n_ar_bank/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '10.0.1.0.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
-    'author': 'ADHOC SA,Moldeo Interactive',
+    'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'summary': '',
     'depends': [

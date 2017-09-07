af41758cfd16a33776a1b596b32c883e83f99247
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-09-07 10:51:41 -0300

    MIG l10n_ar_base

--- l10n_ar_base/__init__.py
+++ l10n_ar_base/__init__.py
@@ -3,7 +3,7 @@
 # For copyright and license notices, see __openerp__.py file in module root
 # directory
 ##############################################################################
-from openerp import SUPERUSER_ID
+from openerp import SUPERUSER_ID, api
 
 from openerp.addons import account
 old_auto_install_l10n = account._auto_install_l10n
@@ -13,15 +13,12 @@ def ar_auto_install_l10n(cr, registry):
     """
     overwrite of this function to install our localization module
     """
-    country_code = registry['res.users'].browse(
-        cr, SUPERUSER_ID, SUPERUSER_ID, {}).company_id.country_id.code
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    country_code = env.user.company_id.country_id.code
     if country_code and country_code == 'AR':
-        module_ids = registry['ir.module.module'].search(
-            cr, SUPERUSER_ID, [
-                ('name', '=', 'l10n_ar_chart'),
-                ('state', '=', 'uninstalled')])
-        registry['ir.module.module'].button_install(
-            cr, SUPERUSER_ID, module_ids, {})
+        env['ir.module.module'].search([
+            ('name', '=', 'l10n_ar_chart'),
+            ('state', '=', 'uninstalled')]).button_install()
     else:
         return old_auto_install_l10n(cr, registry)
 

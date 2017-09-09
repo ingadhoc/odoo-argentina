2218179f285898c49dfdd394b10de5bc07c36fb4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-09-09 11:03:55 -0300

    REF

--- l10n_ar_account/tests/test_reconciliation.py
+++ l10n_ar_account/tests/test_reconciliation.py
@@ -3,6 +3,8 @@ from openerp.addons.account.tests.test_reconciliation import TestReconciliation
 
 
 def new_method(self):
+    # TODO analizar bien porque este test da error con este modulo, ajustarlo
+    # y re activarlo
     pass
 
 

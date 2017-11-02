27500036528a73f128305bc5f1cd6865447b0544
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-11-02 12:23:48 -0300

    IMP for tax settlement and demo data

--- l10n_ar_afipws_fe/__openerp__.py
+++ l10n_ar_afipws_fe/__openerp__.py
@@ -9,14 +9,14 @@
     'summary': '',
     'depends': [
         'l10n_ar_afipws',
-        # 'l10n_ar_account',
+        'l10n_ar_account',
         # TODO improove this, we add this dependency because of demo data only
         # becuase demo data needs de chart account installed, we should
         # take this data tu l10n_ar_chart and set electronic if available
         # we try adding this demo data on l10n_ar_chart but if done, we should
         # add dependency to this module and also duplicate invoice data so
         # it does not use same id
-        'l10n_ar_chart',
+        # 'l10n_ar_chart',
     ],
     'external_dependencies': {
     },
@@ -36,11 +36,11 @@
         'security/ir.model.access.csv',
     ],
     'demo': [
-        'demo/account_journal_expo_demo.yml',
-        '../l10n_ar_account/demo/account_customer_expo_invoice_demo.yml',
-        'demo/account_journal_demo.yml',
-        '../l10n_ar_account/demo/account_customer_invoice_demo.yml',
-        'demo/account_journal_demo_without_doc.yml',
+        # 'demo/account_journal_expo_demo.yml',
+        # '../l10n_ar_account/demo/account_customer_expo_invoice_demo.yml',
+        # 'demo/account_journal_demo.yml',
+        # '../l10n_ar_account/demo/account_customer_invoice_demo.yml',
+        # 'demo/account_journal_demo_without_doc.yml',
     ],
     'test': [
     ],
--- l10n_ar_chart/__openerp__.py
+++ l10n_ar_chart/__openerp__.py
@@ -23,27 +23,28 @@
         # 'data/account_chart_template.yml',
     ],
     'demo': [
-        # para datos demo agregamos alicuotas a las percepciones aplicadas y
-        # sufridas
-        '../l10n_ar_account/demo/account_tax_template_demo.xml',
-        'data/account_chart_template.yml',
-        # TODO los productos se podrian cargar directamente en l10n_ar_account
-        '../l10n_ar_account/demo/product_product_demo.xml',
-        '../l10n_ar_account/demo/account_customer_invoice_demo.yml',
-        '../l10n_ar_account/demo/account_customer_expo_invoice_demo.yml',
-        '../l10n_ar_account/demo/account_customer_invoice_validate_demo.yml',
-        '../l10n_ar_account/demo/account_customer_refund_demo.yml',
-        '../l10n_ar_account/demo/account_supplier_invoice_demo.yml',
-        '../l10n_ar_account/demo/account_supplier_refund_demo.yml',
-        # todo ver si usamos esto o un demo con el de groups
-        # '../l10n_ar_account/demo/account_payment_demo.yml',
-        '../l10n_ar_account/demo/account_other_docs_demo.yml',
-        # we add this file only fot tests run by odoo, we could use
-        # an yml testing if config.options['test_enable'] and only load it
-        # in that case
-        '../l10n_ar_account/demo/account_journal_demo.xml',
-        # '../account/demo/account_bank_statement.yml',
-        # '../account/demo/account_invoice_demo.yml',
+        # MOVEMOS demo data a modulo custom para poder cargar otros datos demo
+        # # para datos demo agregamos alicuotas a las percepciones aplicadas y
+        # # sufridas
+        # '../l10n_ar_account/demo/account_tax_template_demo.xml',
+        # 'data/account_chart_template.yml',
+        # # TODO productos se podrian cargar directamente en l10n_ar_account
+        # '../l10n_ar_account/demo/product_product_demo.xml',
+        # '../l10n_ar_account/demo/account_customer_invoice_demo.yml',
+        # '../l10n_ar_account/demo/account_customer_expo_invoice_demo.yml',
+        # '../l10n_ar_account/demo/account_customer_invoice_validate_demo.yml',
+        # '../l10n_ar_account/demo/account_customer_refund_demo.yml',
+        # '../l10n_ar_account/demo/account_supplier_invoice_demo.yml',
+        # '../l10n_ar_account/demo/account_supplier_refund_demo.yml',
+        # # todo ver si usamos esto o un demo con el de groups
+        # # '../l10n_ar_account/demo/account_payment_demo.yml',
+        # '../l10n_ar_account/demo/account_other_docs_demo.yml',
+        # # we add this file only fot tests run by odoo, we could use
+        # # an yml testing if config.options['test_enable'] and only load it
+        # # in that case
+        # '../l10n_ar_account/demo/account_journal_demo.xml',
+        # # '../account/demo/account_bank_statement.yml',
+        # # '../account/demo/account_invoice_demo.yml',
     ],
     'installable': True,
     'images': [

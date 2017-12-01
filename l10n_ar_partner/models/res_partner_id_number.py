d7f0e43ff1c3390e9c0523101e91ee4f35d82e8c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-12-01 15:19:37 -0300

    IMP compatibility of unique id number with website

--- l10n_ar_partner/models/res_partner_id_number.py
+++ l10n_ar_partner/models/res_partner_id_number.py
@@ -24,13 +24,15 @@ class ResPartnerIdNumber(models.Model):
             related_partners = rec.partner_id.search([
                 '|', ('id', 'parent_of', rec.partner_id.id),
                 ('id', 'child_of', rec.partner_id.id)])
-            if rec.search([
-                    ('name', '=', rec.name),
-                    ('category_id', '=', rec.category_id.id),
-                    # por ahora no queremos la condicion de igual cia
-                    # ('company_id', '=', rec.company_id.id),
-                    ('partner_id', 'not in', related_partners.ids),
-                    ('id', '!=', rec.id)]):
+            same_id_numbers = rec.search([
+                ('name', '=', rec.name),
+                ('category_id', '=', rec.category_id.id),
+                # por ahora no queremos la condicion de igual cia
+                # ('company_id', '=', rec.company_id.id),
+                ('partner_id', 'not in', related_partners.ids),
+                # ('id', '!=', rec.id),
+            ]) - rec
+            if same_id_numbers:
                 raise UserError(_(
                     'Id Number must be unique per id category!\nSame number '
                     'is only allowed for partner with parent/child relation'))

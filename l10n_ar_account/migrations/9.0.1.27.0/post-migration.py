1d67c1a9ae695943d6897d50d473268826a8d1a7
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-02-26 19:07:26 -0300

    FIX invoice currency rates

---
+++ l10n_ar_account/migrations/9.0.1.27.0/post-migration.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate(use_env=True)
+def migrate(env, version):
+    # fix de un error de migración que producia que no se setee esto al migrar
+    for company in env['res.company'].search([]):
+        # arreglamos facturas en otras monedas
+        invoices = env['account.invoice'].search([
+            ('currency_rate', '=', 0.0),
+            ('currency_id', '!=', company.currency_id.id),
+            ('company_id', '=', company.id),
+            ('company_id.localization', '=', 'argentina')])
+        for inv in invoices:
+            inv.currency_rate = inv.get_localization_invoice_vals().get(
+                'currency_rate')
+        # arreglamos facturas en ars
+        env['account.invoice'].search([
+            ('currency_id', '=', company.currency_id.id),
+            ('currency_rate', '=', 0.0),
+            ('company_id', '=', company.id),
+            ('company_id.localization', '=', 'argentina')]).write({
+                'currency_rate': 1.0})

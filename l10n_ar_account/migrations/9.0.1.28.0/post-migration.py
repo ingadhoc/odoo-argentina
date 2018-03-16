8281068df21f1cec8d214e4fa0b3b1e39bec2494
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-03-16 14:28:03 -0300

    [IMP] ADD migration scripts

---
+++ l10n_ar_account/migrations/9.0.1.28.0/post-migration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    openupgrade.load_data(
+        cr, 'l10n_ar_account',
+        'migrations/9.0.1.28.0/account.document.type.csv')

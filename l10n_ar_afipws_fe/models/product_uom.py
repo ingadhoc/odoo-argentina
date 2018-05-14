739e29388a531c1ca36fbb7332010663ec694f89
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-05-13 21:01:39 -0300

    [REL] replace __openerp__ for __manifest__

--- l10n_ar_afipws_fe/models/product_uom.py
+++ l10n_ar_afipws_fe/models/product_uom.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
+# For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
 from openerp import models, api, _

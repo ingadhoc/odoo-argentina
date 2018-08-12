f2dc23c00c9838919da8d3ac77e096e7cb0ba664
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-08-12 20:24:02 -0300

    [REF]

--- l10n_ar_account/models/account_invoice_line.py
+++ l10n_ar_account/models/account_invoice_line.py
@@ -42,6 +42,10 @@ class AccountInvoiceLine(models.Model):
         """For recs of companies with company_requires_vat (that comes from
         the responsability), we ensure one and only one vat tax is configured
         """
+        # por ahora, para no romper los tests de odoo y datos demo de algunos
+        # modulos, lo desactivamos en la instalacion
+        if self._context.get('install_mode'):
+            return True
         for rec in self.filtered('company_id.company_requires_vat'):
             vat_taxes = rec.invoice_line_tax_ids.filtered(
                 lambda x:

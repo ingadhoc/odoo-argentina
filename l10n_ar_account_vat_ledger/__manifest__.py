b2f9692b01d176599b888ccd403371391d5399fd
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-09-04 11:01:02 -0300

    [ADD] scripts for l10n_ar_account_vat_ledger

--- l10n_ar_account_vat_ledger/__manifest__.py
+++ l10n_ar_account_vat_ledger/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian VAT Ledger Management",
-    'version': '9.0.1.7.0',
+    'version': '11.0.10.0.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',

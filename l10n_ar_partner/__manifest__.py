b6c5266cabedd64fb89c4ed3b81410189c87a683
Katherine Zaoral <zaoral@users.noreply.github.com>
2018-10-24 10:33:43 -0300

    [ADD] l10n_ar_partner: portal views content AR partner info. (#268)
    * [ADD] l10n_ar_partner: portal views content AR partner info.
    
    * [ADD] l10n_ar_partner: Billing information section
    
    This is a separate section to the end of the form.
    
    * [ADD] l10n_ar_partner: improves to company_name
    
    Company_name is part of billing information and only visible if the partner
    has a parent partner which is a company
    
    * [FIX] l10n_ar_partner: Fix pylint errors

--- l10n_ar_partner/__manifest__.py
+++ l10n_ar_partner/__manifest__.py
@@ -26,5 +26,5 @@
     'demo': [
         'demo/partner_demo.xml',
     ],
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
 }
--- l10n_ar_partner/controllers/portal.py
+++ l10n_ar_partner/controllers/portal.py
@@ -2,20 +2,71 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from odoo import _
 from odoo.http import request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
-
-CustomerPortal.OPTIONAL_BILLING_FIELDS += [
-    'main_id_number', 'main_id_category_id']
 
 
 class L10nArCustomerPortal(CustomerPortal):
 
+    OPTIONAL_BILLING_FIELDS = CustomerPortal.OPTIONAL_BILLING_FIELDS + [
+        "commercial_partner_id", "main_id_category_id", "main_id_number",
+        "afip_responsability_type_id",
+    ]
+
+    def details_form_validate(self, data):
+        """ When adding either document_type or document_number, this two
+        should be setted.
+        """
+        error, error_message = super(
+            L10nArCustomerPortal, self).details_form_validate(data)
+
+        main_id_number = data.get('main_id_number', False)
+        main_id_category_id = data.get('main_id_category_id', False)
+        if main_id_category_id and not main_id_number:
+            error['main_id_number'] = 'error'
+            error_message.append(_(
+                'Please add the document number.'))
+        if main_id_number and not main_id_category_id:
+            error['main_id_category_id'] = 'error'
+            error_message.append(_(
+                'Please add the type of document.'))
+        return error, error_message
+
     @route()
     def account(self, redirect=None, **post):
+        post_process = False
+        if post:
+            error, _error_message = self.details_form_validate(post)
+            if not error:
+                commercial_partner_id = post.pop(
+                    'commercial_partner_id', False)
+                if commercial_partner_id:
+                    post_process = True
+                    commercial_values = dict(
+                        main_id_number=post.pop('main_id_number'),
+                        main_id_category_id=post.pop('main_id_category_id'),
+                        afip_responsability_type_id=post.pop(
+                            'afip_responsability_type_id'),
+                    )
+
         response = super(L10nArCustomerPortal, self).account(
             redirect=redirect, **post)
         document_categories = request.env[
             'res.partner.id_category'].sudo().search([])
-        response.qcontext.update({'document_categories': document_categories})
+        afip_responsabilities = request.env[
+            'afip.responsability.type'].sudo().search([])
+        uid = request.session.uid
+        partner = request.env['res.users'].browse(uid).partner_id if uid else \
+            request.env['res.partner']
+        partner = partner.with_context(show_address=1).sudo()
+        response.qcontext.update({
+            'document_categories': document_categories,
+            'afip_responsabilities': afip_responsabilities,
+            'partner': partner,
+            })
+
+        if post_process:
+            partner.commercial_partner_id.write(commercial_values)
+
         return response

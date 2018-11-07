99fd9c0ff647cd8eae6f3b31347515a564014315
ValentinoDefelice <vd@adhoc.com.ar>
2018-11-07 09:20:43 -0300

    [Merge] Port-forward from 9.0. (#278)
    * [FIX] move the part of code who change the location of field ref to another view inherit with base odoo view (#277)

--- l10n_ar_account/__manifest__.py
+++ l10n_ar_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Módulo base de Contabilidad Argentina",
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.2.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',

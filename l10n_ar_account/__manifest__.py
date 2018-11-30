7ca633fa72405a17d7b718f3add69794acd15c70
Juan José Scarafía <jjs@adhoc.com.ar>
2018-11-30 15:55:40 -0300

    [ADD] tag_unaffected_earnings (#287)

--- l10n_ar_account/__manifest__.py
+++ l10n_ar_account/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Módulo base de Contabilidad Argentina",
-    'version': '11.0.1.5.1',
+    'version': '11.0.1.6.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',

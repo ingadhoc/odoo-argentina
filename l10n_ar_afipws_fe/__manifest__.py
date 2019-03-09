664c219abc99160a76b0eee1cefa316bbc8ab102
Katherine Zaoral <zaoral@users.noreply.github.com>
2019-03-09 07:23:25 -0300

    [ADD] l10n_ar_afipws_fe: Manage error msg when certificate is not defined (#308)
    * [ADD] l10n_ar_afipws_fe: Manage error msg when certificate is not defined
    
    * [FIX] l10n_ar_account/l10n_ar_afipws_fe: translations

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Factura Electrónica Argentina",
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.1.1',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)',

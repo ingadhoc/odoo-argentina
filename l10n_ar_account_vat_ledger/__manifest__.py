b91673e98cab0c02395373e93338a889488926be
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-04-30 14:48:37 -0300

    [ADD] vat ledger on columns

--- l10n_ar_account_vat_ledger/__manifest__.py
+++ l10n_ar_account_vat_ledger/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian VAT Ledger Management",
-    'version': '11.0.11.0.0',
+    'version': '11.0.11.1.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',
@@ -15,8 +15,8 @@
     },
     "data": [
         'data/l10n_ar_account_vat_ledger_data.xml',
-        'views/account_vat_report_views.xml',
         'report/account_vat_ledger_report.xml',
+        'views/account_vat_report_views.xml',
         'wizards/res_config_settings_views.xml',
         'security/security.xml',
         'security/ir.model.access.csv',

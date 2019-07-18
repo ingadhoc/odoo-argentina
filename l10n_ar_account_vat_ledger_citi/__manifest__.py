8c87a96ce8b31178b073bb19b1c07f88be543ef9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-07-18 12:45:43 -0300

    [IMP] add citi skip test and lines

--- l10n_ar_account_vat_ledger_citi/__manifest__.py
+++ l10n_ar_account_vat_ledger_citi/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinian CITI Reports",
-    'version': '9.0.1.3.0',
+    'version': '9.0.1.4.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA,Moldeo Interactive,Odoo Community Association (OCA)',

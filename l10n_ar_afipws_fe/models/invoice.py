0247d3e2ed7d0879b5402f92a60796073d75a468
Mario <mariodmorenosoltic@gmail.com>
2019-09-02 12:02:04 -0300

    [IMP] add FCE comercial reference (#340)
    * [IMP] add FCE comercial reference
    
    * [FIX] afip_reference is replaced by name
    
    * [FIX]

--- l10n_ar_afipws_fe/models/invoice.py
+++ l10n_ar_afipws_fe/models/invoice.py
@@ -620,6 +620,10 @@ print "Observaciones:", wscdc.Obs
                     ws.AgregarOpcional(
                         opcional_id=2101,
                         valor=inv.partner_bank_id.cbu)
+                    if inv.name:
+                        ws.AgregarOpcional(
+                            opcional_id=23,
+                            valor=inv.name)
                 elif int(doc_afip_code) in [202, 203, 207, 208, 212, 213]:
                     # si es una NC y si el valor es el mismo al comprobante original entonces es una anulacion
                     if int(doc_afip_code) in [203, 208, 213] and CbteAsoc.amount_total == self.amount_total:

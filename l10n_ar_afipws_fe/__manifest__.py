60a7fcfc38d4a849f0102a5ab83613649ff79ef1
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-10-09 18:50:25 -0300

    [REF] increase manifest version

--- l10n_ar_afipws_fe/__manifest__.py
+++ l10n_ar_afipws_fe/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Factura Electrónica Argentina",
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.3.0',
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA, Moldeo Interactive,Odoo Community Association (OCA)',

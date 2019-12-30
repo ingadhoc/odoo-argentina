6f8336b7cc130f0b10c8497eb18245a06b3c95c9
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2019-12-30 10:26:42 -0300

    [12.0] [FIX] l10n_ar_account: changes in refund invoices (#379)
    * [12.0] [FIX] l10n_ar_account: changes in refund invoices
    
    The changes apply in the module AccountInvoiceRefund, it's not working for the state "cancel" or "modifify and both. For this now we change in the method who set the vals of the refund before create them.
    
    * [12.0] [FIX] l10_ar_afips_fe: Fix wrong identation

--- l10n_ar_account/wizard/__init__.py
+++ l10n_ar_account/wizard/__init__.py
@@ -2,6 +2,5 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import account_invoice_refund
 from . import res_partner_update_from_padron_wizard
 from . import res_config_settings

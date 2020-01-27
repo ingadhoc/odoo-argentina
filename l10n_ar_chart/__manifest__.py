c950554e47b066d8bdb3b11ee97a6720bd07a8c5
Katherine Zaoral <kz@adhoc.com.ar>
2020-01-27 17:20:10 -0300

    clean up
    - remove account group data definition
    - remove the chart template data definiton
    * remove fiscal positions
    - delete commented code
    - remove demo data for installing demo data.
    - remove icon

--- l10n_ar_chart/__manifest__.py
+++ l10n_ar_chart/__manifest__.py
@@ -10,28 +10,11 @@
         'account_check',
     ],
     'data': [
-        'data/account_group_data.xml',
-        'data/account_chart_template_data.xml',
         'data/account.account.template.csv',
-        'data/account_chart_template_data2.xml',
-
         'data/account_chart_template.xml',
-        'data/account_tax_template.xml',
         'data/account_tax_withholding_template.xml',
-        'data/account_fiscal_template.xml',
     ],
     'demo': [
-        # MOVEMOS demo data a modulo custom para poder cargar otros datos demo
-        # # para datos demo agregamos alicuotas a las percepciones aplicadas y
-        # # sufridas
-        # '../l10n_ar_account/demo/account_tax_template_demo.xml',
-        # # TODO los productos se podrian cargar en l10n_ar_account
-        # '../l10n_ar_account/demo/product_product_demo.xml',
-        # '../l10n_ar_account/demo/account_tax_template_demo.xml',
-        # # we add this file only fot tests run by odoo, we could use
-        # # an yml testing if config.options['test_enable'] and only load it
-        # # in that case
-        # '../l10n_ar_account/demo/account_journal_demo.xml',
     ],
     'installable': True,
     'images': [

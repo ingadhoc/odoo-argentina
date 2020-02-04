b2ed50eeac691567a14ac979adc201f6644e6636
Katherine Zaoral <zaoral@users.noreply.github.com>
2020-02-04 11:38:46 -0300

    [ADD] l10n_ar_ux: new module (#393)
    * [ADD] l10n_ar_ux: new module
    
    Extend Argentinian official localization (l10n_ar) to add usability
    improvements
    
    * [FIX] travis
    
    Co-authored-by: Juan José Scarafía <jjs@adhoc.com.ar>

---
+++ l10n_ar_ux/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Argentinian Localization UX',
+    'version': "13.0.1.0.0",
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA,Odoo Community Association (OCA)',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'l10n_ar',
+    ],
+    'data': [
+        'data/res_currency_data.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': True,
+}

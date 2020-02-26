76412eec71b57dec3a267494f2ee90e6e62aaaa5
Katherine Zaoral <kz@adhoc.com.ar>
2020-02-26 11:21:11 -0300

    remove translations and hook

--- l10n_ar_chart/__init__.py
+++ l10n_ar_chart/__init__.py
@@ -4,9 +4,3 @@
 ##############################################################################
 from . import models
 from odoo import SUPERUSER_ID
-
-
-def load_translations(cr, registry):
-    chart_template = registry['ir.model.data'].xmlid_to_object(
-        cr, SUPERUSER_ID, 'l10n_be.l10nbe_chart_template')
-    chart_template.process_coa_translations()

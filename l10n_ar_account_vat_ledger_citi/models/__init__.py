71ef1882d4d741b304b173f6321c26b618be09a3
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-04-26 16:30:07 -0300

    [REF] split repository into odoo-argentina-ce

--- l10n_ar_account_vat_ledger_citi/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_vat_ledger
-from . import account_document_type
--- l10n_ar_afipws/models/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import afipws_certificate_alias
-from . import afipws_certificate
-from . import afipws_connection
-from . import res_company

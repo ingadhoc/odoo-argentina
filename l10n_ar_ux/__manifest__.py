dcc2dc3cf9e8e00244339295a921b59eea229843
rhe-mastercore <rhe@mastercore.net>
2021-01-03 23:32:06 -0400

    [FIX] l10n_ar_ux: Bugfix in view domain

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "13.0.1.9.0",
+    'version': "13.0.1.10.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

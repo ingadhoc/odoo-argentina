a2ef064a75b510c5741799ca273fc1e0d11fe7ff
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-02-04 18:06:50 -0300

    [13.0] [IMP] l10n_ar_ux: Optional group to hide or show documents related in recipts.

--- l10n_ar_ux/wizards/__init__.py
+++ l10n_ar_ux/wizards/__init__.py
@@ -2,8 +2,4 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import controllers
-from . import models
-from . import reports
-from . import wizards
-from .hooks import post_init_hook
+from . import res_config_settings

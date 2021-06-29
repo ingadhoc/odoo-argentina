9147061256fa4802e47a27559e1c476f333d46aa
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-29 08:55:06 -0300

    [13.0] [FIX] l10n_ar_ux: Add amount in currency to checks and witholding lines.

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "13.0.1.22.0",
+    'version': "13.0.1.23.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

6d887f53311e906b6d35c0e75d1301239beeab6a
raguero </SUlem@2017/>
2021-11-29 09:26:37 -0300

    [MIG] l10n_ar_ux: Migration to 15.0

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "13.0.1.32.0",
+    'version': "15.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

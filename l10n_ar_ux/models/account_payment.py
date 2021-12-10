6b13375eb22e7c5419dc0b9115f1c957bd308851
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-10 09:50:47 -0300

    [FIX] l10n_ar_ux: checks integration

--- l10n_ar_ux/models/account_payment.py
+++ l10n_ar_ux/models/account_payment.py
@@ -5,18 +5,3 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     l10n_ar_partner_vat = fields.Char(related='partner_id.l10n_ar_vat', string='CUIT del destinatario')
-
-    @api.model
-    def _get_trigger_fields_to_sincronize(self):
-        res = super()._get_trigger_fields_to_sincronize()
-        return res + ('check_payment_date',)
-
-    def _prepare_move_line_default_vals(self, write_off_line_vals=None):
-        """ Add check name and operation on liquidity line """
-        res = super()._prepare_move_line_default_vals(write_off_line_vals=write_off_line_vals)
-        check = self if self.payment_method_line_id.code == 'new_third_checks' else self.check_id
-        if check:
-            res[0].update({
-                'date_maturity': check.check_payment_date or self.date,
-            })
-        return res

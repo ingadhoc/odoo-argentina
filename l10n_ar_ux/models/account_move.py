c55302e13d991defa271f1fb75a722b539d9fe59
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-12-13 10:46:37 +0000

    [IMP] l10n_ar_ux: re-add check
    Part-of: ingadhoc/odoo-argentina#570

--- l10n_ar_ux/models/account_move.py
+++ l10n_ar_ux/models/account_move.py
@@ -85,15 +85,12 @@ class AccountMove(models.Model):
         return super(
             AccountMove, self.filtered(lambda x: not x.l10n_latam_use_documents))._check_duplicate_supplier_reference()
 
-    def _get_name_invoice_report(self, report_xml_id):
+    def _get_name_invoice_report(self):
         """Use always argentinian like report (regardless use documents)"""
         self.ensure_one()
         if self.company_id.country_id.code == 'AR':
-            custom_report = {
-                'account.report_invoice_document': 'l10n_ar.report_invoice_document',
-            }
-            return custom_report.get(report_xml_id) or report_xml_id
-        return super()._get_name_invoice_report(report_xml_id)
+            return 'l10n_ar.report_invoice_document'
+        return super()._get_name_invoice_report()
 
     def _get_l10n_latam_documents_domain(self):
         self.ensure_one()

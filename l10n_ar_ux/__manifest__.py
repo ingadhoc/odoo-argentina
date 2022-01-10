3b21797b6a8beabf51bf9ca2e5c693d716102024
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-01-10 12:30:44 +0000

    [IMP] l10n_ar_ux: partner always configurable on journal
    Como estamos usando reporte argentino en todos los diarios (compra y venta) de cias argentinas, dejamos que sea seleccionable el partner.
    
    closes ingadhoc/odoo-argentina#574
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "13.0.1.32.0",
+    'version': "13.0.1.33.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

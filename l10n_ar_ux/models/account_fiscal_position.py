cae3412a11a290ced25c83feb6978cb1c35d536f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-01-13 09:00:20 -0300

    Revert "[IMP] l10n_ar_ux: better fiscal positions auto-detect"
    This reverts commit 02b149d568148fd5ae5dd65c608d599980e36d6d.

--- l10n_ar_ux/models/account_fiscal_position.py
+++ l10n_ar_ux/models/account_fiscal_position.py
@@ -8,9 +8,7 @@ def get_fiscal_position(self, partner_id, delivery_id=None):
     company = self.env.company
     if company.country_id.code == "AR":
         partner = self.env['res.partner'].browse(partner_id)
-        self = self.with_context(
-            company_code='AR',
-            l10n_ar_afip_responsibility_type_id=partner.l10n_ar_afip_responsibility_type_id.id)
+        self = self.with_context(l10n_ar_afip_responsibility_type_id=partner.l10n_ar_afip_responsibility_type_id.id)
     return super(AccountFiscalPosition, self).get_fiscal_position(partner_id, delivery_id=delivery_id)
 
 
@@ -24,17 +22,12 @@ class AccountFiscalPositionMp(models.Model):
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         """ Take into account the partner afip responsibility in order to auto-detect the fiscal position """
-        if self._context.get('company_code') == 'AR':
-            args += [('l10n_ar_afip_responsibility_type_ids', '=', self._context.get('l10n_ar_afip_responsibility_type_id'))]
+        if 'l10n_ar_afip_responsibility_type_id' in self._context:
+            args += [
+                '|',
+                ('l10n_ar_afip_responsibility_type_ids', '=', False),
+                ('l10n_ar_afip_responsibility_type_ids', '=', self._context.get('l10n_ar_afip_responsibility_type_id'))]
         return super()._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
 
     def _onchange_afip_responsibility(self):
         return {}
-
-    @api.model
-    def _get_fpos_by_region(self, country_id=False, state_id=False, zipcode=False, vat_required=False):
-        if country_id and 'website_id' in self._context and 'l10n_ar_afip_responsibility_type_id' not in self._context:
-            company = self.env['res.company'].browse(self._context.get('force_company', self.env.company.id))
-            if company.country_id.code == 'AR':
-                self = self.with_context(company_code='AR')
-        return super()._get_fpos_by_region(country_id=country_id, state_id=state_id, zipcode=zipcode, vat_required=vat_required)

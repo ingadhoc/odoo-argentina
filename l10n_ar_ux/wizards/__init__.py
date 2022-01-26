9b8c2a911e66f8873e178868b54cd6ea2d87eb11
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-01-26 15:02:54 -0300

    [ADD] l10n_ar_ux: add multiple checks

--- l10n_ar_ux/wizards/__init__.py
+++ l10n_ar_ux/wizards/__init__.py
@@ -5,3 +5,4 @@
 from . import res_config_settings
 from . import account_move_change_rate
 from . import account_payment_register
+from . import account_payment_add_checks
---
+++ l10n_ar_ux/wizards/account_payment_add_checks.py
@@ -0,0 +1,26 @@
+
+from odoo import fields, models
+
+
+class AccounTpaymentAddChecks(models.TransientModel):
+    _name = "account.payment.add.checks"
+    _description = "account.payment.add.checks"
+
+    check_ids = fields.Many2many(comodel_name="account.payment")
+
+    def confirm(self):
+        self.ensure_one()
+        payment_group = self.env["account.payment.group"].browse(self.env.context.get("active_id", False))
+        if payment_group:
+            vals_list = [{
+                'l10n_latam_check_id': check.id,
+                'amount': check.amount,
+                'payment_group_id': payment_group.id,
+                'payment_type': 'outbound',
+                'journal_id': check.l10n_latam_check_current_journal_id.id,
+                'payment_method_line_id': check.l10n_latam_check_current_journal_id._get_available_payment_method_lines(
+                    'oubound').filtered(lambda x: x.code == 'out_third_checks').id,
+            } for check in self.check_ids]
+            self.env['account.payment'].create(vals_list)
+
+        return {"type": "ir.actions.act_window_close"}

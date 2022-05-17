8aa4cbdb1d086bcaf9d9cb26f508146a03cbde20
Pablo Montenegro <pam@adhoc.com.ar>
2022-05-17 14:27:10 +0000

    [IMP]l10n_ar_ux: allow setting day currency rate
    closes ingadhoc/odoo-argentina#613
    
    Ticket: 51807
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "15.0.1.9.0",
+    'version': "15.0.1.10.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

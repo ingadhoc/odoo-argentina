eac89b0d07f9d6b95b599b3d1ca29c4175489f36
matiasperalta1 <matias_peralta1@hotmail.com>
2022-09-15 11:27:22 -0300

    commit 1

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ Translation Status
 
 <img alt="ADHOC" src="http://fotos.subefotos.com/83fed853c1e15a8023b86b2b22d6145bo.png" />
 **Adhoc SA** - www.adhoc.com.ar
+**Adhoc SA** - www.adhoc.com.ar
+**Adhoc SA** - www.adhoc.com.ar
--- l10n_ar_ux/__init__.py
+++ l10n_ar_ux/__init__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
-# directory
+# directoryytyrtyrty
 ##############################################################################
 from . import controllers
 from . import models

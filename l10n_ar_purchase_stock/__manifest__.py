85f7738c98b6f80706972b82be27a2c4d2e6e4ae
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-10-04 14:26:44 -0300

    [ADD] l10n_ar_purchase_stock

---
+++ l10n_ar_purchase_stock/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Argentinian Purchase Stock',
+    'version': "15.0.1.0.0",
+    'category': 'Localization/Argentina',
+    'sequence': 14,
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'purchase_stock',
+        'l10n_ar_purchase',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/purchase_report_templates.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}

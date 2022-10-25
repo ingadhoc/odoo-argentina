39ee6fe50b10cba816f6b8b615ccf5367de53ff7
Pablo Montenegro <pam@adhoc.com.ar>
2022-10-25 18:19:47 +0000

    [FIX] l10n_ar_ux: post invoice with l10n_ar_currency_rate
    closes ingadhoc/odoo-argentina#643
    
    Ticket: 56756
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_ux/models/account_move_line.py
+++ l10n_ar_ux/models/account_move_line.py
@@ -6,6 +6,7 @@ class AccountMoveLine(models.Model):
 
     @api.onchange('currency_id', 'amount_currency')
     def _onchange_currency(self):
+        """This method is a copy of the original one in account module, here we use the same logic if l10n_ar_currency_rate y setted"""
         force_currency_rate_lines = self.filtered(lambda x: x.move_id.l10n_ar_currency_rate)
         for line in force_currency_rate_lines:
             company = line.move_id.company_id
@@ -13,7 +14,7 @@ class AccountMoveLine(models.Model):
                 line.with_context(force_rate=line.move_id.l10n_ar_currency_rate)._onchange_price_subtotal()
             elif not line.move_id.reversed_entry_id:
                 balance = line.currency_id.with_context(force_rate=line.move_id.l10n_ar_currency_rate)._convert(line.amount_currency, company.currency_id, company, line.move_id.date or fields.Date.context_today(line))
-            line.debit = balance if balance > 0.0 else 0.0
-            line.credit = -balance if balance < 0.0 else 0.0
+                line.debit = balance if balance > 0.0 else 0.0
+                line.credit = -balance if balance < 0.0 else 0.0
 
         super(AccountMoveLine, self - force_currency_rate_lines)._onchange_currency()

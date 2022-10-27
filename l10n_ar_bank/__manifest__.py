5b4063d6b057e4a7d35c1a061e9887786f7d1f0f
Maximiliano Mezzavilla <mem@adhoc.com>
2022-10-27 18:24:50 +0000

    [MIG] l10n_ar_bank: Migration to 16.0
    closes ingadhoc/odoo-argentina#645
    
    Signed-off-by: Pablo  Montenegro <pam@adhoc.com.ar>

--- l10n_ar_bank/__manifest__.py
+++ l10n_ar_bank/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Listado de Bancos Argentinos",
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -12,7 +12,7 @@
     'data': [
         'data/res_bank.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }

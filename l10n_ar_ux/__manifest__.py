6621eae838d960b317ab82348f671e6dd14e0722
Ignacio Cainelli <ica@adhoc.com.ar>
2022-11-01 16:26:38 -0300

    [FIX] l10n_ar_ux: payment group report check data
    Now the data of checks prints correctly in the payment group report

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "15.0.1.11.0",
+    'version': "15.0.1.12.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

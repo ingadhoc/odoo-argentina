a36e8b73259301161404df675d39f71be041d996
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-11-09 13:10:45 -0300

    [REM] l10n_ar_ux: checkbooks printing funcitonality Las chequeras fueron deprecadas en el modulo nativo de odoo Vamos a probar depreciar tmb esta funcionalidad y luego si es necesaria vemos de re-incorporarla.

--- l10n_ar_ux/wizards/account_payment_register.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import models, fields, api, _
-
-
-class AccountPaymentRegister(models.TransientModel):
-    _inherit = 'account.payment.register'
-
-    l10n_latam_check_printing_type = fields.Selection(related='l10n_latam_checkbook_id.check_printing_type')

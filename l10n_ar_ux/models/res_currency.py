16a3044a0a8f81c21f794e8636e1e64bb7c6e4c8
Katherine Zaoral <kz@adhoc.com.ar>
2023-02-22 20:58:11 +0000

    [IMP] l10n_ar_ux: search currenct by afip code
    closes ingadhoc/odoo-argentina#681
    
    Signed-off-by: pablohmontenegro <pam@adhoc.com.ar>

--- l10n_ar_ux/models/res_currency.py
+++ l10n_ar_ux/models/res_currency.py
@@ -8,3 +8,9 @@ class ResCurrency(models.Model):
     def _get_conversion_rate(self, from_currency, to_currency, company, date):
         return self._context.get('force_rate', False) or super()._get_conversion_rate(
             from_currency, to_currency, company, date)
+
+    @api.model
+    def _search_by_name(self, currency_name):
+        """ This method was original defined in ingadhoc/enterprise-extensions/account_balance_import.
+        We overwrited completely in order to add the logic of AFIP Code """
+        return self.search(["|", ("name", "=", currency_name), ("l10n_ar_afip_code", "=", currency_name)])

04a155b2c61a18f35ddb74f47b292beae1345821
Maximiliano Mezzavilla <mem@adhoc.com>
2023-04-05 14:12:07 -0300

    [FIX] l10n_ar_ux: No existe mas el campo l10n_latam_checkbook_id Ticket: 60649, 60341

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "16.0.1.1.0",
+    'version': "16.0.1.1.1",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

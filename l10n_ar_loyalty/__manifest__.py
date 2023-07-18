0577f911ef3e95501264aebb5ae55553a57a0b32
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2023-07-18 19:34:28 +0000

    [ADD] l10n_ar_loyalty: Nuevo modelo para productos de bonificacion
    closes ingadhoc/odoo-argentina#715
    
    Task: 32083
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

---
+++ l10n_ar_loyalty/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'l10n_ar Loyalty',
+    'version': "16.0.1.0.0",
+    'description': '',
+    'author': 'ADHOC SA',
+    'depends': [
+        'l10n_ar',
+        'loyalty',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'assets': {
+    },
+    'license': 'LGPL-3',
+}

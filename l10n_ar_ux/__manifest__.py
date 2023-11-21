7ef20afe5d725dc70937152158ba05dae36d88a9
Juan José Scarafia <jjs@adhoc.com.ar>
2023-11-21 16:30:30 -0300

    [MIG] l10n_ar_ux/: Migration to 17.0

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "17.0.0.0.0",
+    'version': "17.0.1.0.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

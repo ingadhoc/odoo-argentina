114837ac88f6e5bd12bd1abcb39d83fcc3ab859b
Luciano <lucianobaleani94@gmail.com>
2024-02-08 14:42:23 -0300

    [ADD][l10n_ar_withholding_certificate]

---
+++ l10n_ar_withholding_certificate_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Argentinian Withholding Certification Report",
+    "version": "16.0.0.0.0",
+    "category": "Accounting",
+    "summary": "Adding new information to the Argentinian Withholding Certification Report ",
+    "author": "Axcelere SA",
+    "website": "https://www.axcelere.com",
+    "license": "LGPL-3",
+    "depends": ["l10n_ar_account_withholding"],
+    "data": [
+            "reports/report_withholding_certificate_inherited.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}

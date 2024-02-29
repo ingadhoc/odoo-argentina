b75a9b00c1afd83dd74e9f3154c6237c3bd829e1
Rocío Vega <rov@adhoc.com.ar>
2024-02-29 15:45:02 +0000

    [FIX] l10n_ar_ux: fix in order to keep the original date if it exists
    closes ingadhoc/odoo-argentina#811
    
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- l10n_ar_ux/models/account_move.py
+++ l10n_ar_ux/models/account_move.py
@@ -93,7 +93,7 @@ class AccountMove(models.Model):
 
     def _post(self, soft=True):
         """ Estamos sobreescribiendo este método para hacer cosas que en odoo oficial no se puede tanto previo como posterior a la validación de la factura. """
-        ar_invoices = self.filtered(lambda x: x.company_id.account_fiscal_country_id.code == "AR" )
+        ar_invoices = self.filtered(lambda x: x.company_id.account_fiscal_country_id.code == "AR" and x.is_invoice(include_receipts=True))
 
         # Forzamos cambio de fecha en factura para actualizar cotización. Solucionamos problemas de cálculo en apunte contable y actualización de cotización. Solo usamos en l10n_ar. Considerar uso en otras locs. Resuelve:
         #   1. Facturas creadas días atrás y dejadas en borrador usan cotización actual al validar.
@@ -110,7 +110,7 @@ class AccountMove(models.Model):
             invoice_date = inv.invoice_date
             inv.invoice_date = old_date
             inv.invoice_date = invoice_date or today
-
+ 
             if inv.move_type in ['in_invoice', 'in_refund']:
                 accounting_date = inv.date
                 inv.date = old_date

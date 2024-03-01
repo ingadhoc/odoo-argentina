ace25a3c060b617ff5e895560f8d6a36ef741aaf
Juan José Scarafia <jjs@adhoc.com.ar>
2024-03-01 01:13:19 +0000

    [IMP] withholding: minor fix and remove old data
    closes ingadhoc/odoo-argentina#812
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -16,8 +16,6 @@
     'data': [
         'data/res_currency_data.xml',
         'data/account_account_tag_data.xml',
-        # 'data/account_tax_group_data.xml',
-        # 'data/account_tax_template_data.xml',
         'wizards/account_move_change_rate_views.xml',
         'views/portal_templates.xml',
         'views/account_move_view.xml',

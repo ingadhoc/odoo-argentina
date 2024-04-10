47e53b3849c5b980c0c864fe0f5fe0a5a9381ef3
Martin Quinteros <maq@adhoc.com.ar>
2024-04-10 16:45:04 -0300

    [FIX] l10n_ar_withholding_ux : Add monkey patch to clean _synchronize_to_moves

--- l10n_ar_withholding_ux/__init__.py
+++ l10n_ar_withholding_ux/__init__.py
@@ -1,1 +1,9 @@
 from . import models
+
+from odoo.addons.l10n_ar_withholding.models.account_payment import AccountPayment
+
+def monkey_patch_synchronize_to_moves():
+
+    def _synchronize_to_moves(self, changed_fields):
+        return super(AccountPayment, self)._synchronize_to_moves(changed_fields)
+    AccountPayment._synchronize_to_moves = _synchronize_to_moves

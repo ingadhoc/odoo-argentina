f688b323ccaafcc38a5e4aa9763dca69971d84c9
Manuela Cepeda <cem@adhoc.com.ar>
2024-04-26 14:19:09 -0300

    commit 1 y 2

--- l10n_ar_ux/models/res_currency.py
+++ l10n_ar_ux/models/res_currency.py
@@ -9,3 +9,7 @@ class ResCurrency(models.Model):
         """ This method was original defined in ingadhoc/enterprise-extensions/account_balance_import.
         We overwrited completely in order to add the logic of AFIP Code """
         return self.search(["|", ("name", "=", currency_name), ("l10n_ar_afip_code", "=", currency_name)])
+
+
+
+# cambios 3

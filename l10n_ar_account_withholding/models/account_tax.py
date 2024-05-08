57d6035eb2e64908c46c4d5dc0cf9175c3e7e233
Juan José Scarafia <jjs@adhoc.com.ar>
2024-05-08 17:18:25 +0000

    [FIX] _witholding: restore date for partner aliquot
    revertimos esto https://github.com/ingadhoc/odoo-argentina/commit/5fb0ba2221d5c1af6ecf1123d9bf2c8d15bad453
    para tener de nuevo esto que es necesario para que la factura calcule bien https://github.com/ingadhoc/odoo-argentina/commit/bc5eba3ab8bb0e5e4319d19752047b6811332594
    
    X-original-commit: dff9a16d9b1046bb5b889cedee079d01e6382552

--- l10n_ar_account_withholding/models/account_tax.py
+++ l10n_ar_account_withholding/models/account_tax.py
@@ -249,11 +249,18 @@ class AccountTax(models.Model):
             self, base_amount, price_unit, quantity=1.0, product=None, partner=None, fixed_multiplicator=1):
         if self.amount_type == 'partner_tax':
             date = self._context.get('invoice_date', fields.Date.context_today(self))
+<<<<<<< HEAD
 
             # Parche para intentar que al intentar corregir el problema que surge al intentar consultar la alicuota
             # para la fecha que estamos usando para forzar la actualizacion de la cotizacion
             if not date or str(date) == '1970-01-01':
                 date = fields.Date.context_today(self)
+||||||| parent of 47ef581c (temp)
+=======
+
+            if not date:
+                date = fields.Date.context_today(self)
+>>>>>>> 47ef581c (temp)
             partner = partner and partner.sudo()
             return base_amount * self.sudo().get_partner_alicuota_percepcion(partner, date)
         else:

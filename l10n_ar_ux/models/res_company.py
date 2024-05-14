b23ae90557be039f86e16ec199c6934525255a3d
--replace-all <joa@adhoc.com.ar>
2024-05-14 10:46:36 -0300

    [IMP] l10n_ar_ux: Test3 commit editado

--- l10n_ar_ux/models/res_company.py
+++ l10n_ar_ux/models/res_company.py
@@ -1,6 +1,7 @@
 ##############################################################################
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
+#
 ##############################################################################
 from odoo import fields, models
 
--- l10n_ar_ux/models/res_currency.py
+++ l10n_ar_ux/models/res_currency.py
@@ -7,5 +7,5 @@ class ResCurrency(models.Model):
     @api.model
     def _search_by_name(self, currency_name):
         """ This method was original defined in ingadhoc/enterprise-extensions/account_balance_import.
-        We overwrited completely in order to add the logic of AFIP Code """
+        We overwrited completely in order to add the logic of AFIP Code  """
         return self.search(["|", ("name", "=", currency_name), ("l10n_ar_afip_code", "=", currency_name)])

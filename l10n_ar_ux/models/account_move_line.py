857af1ca325ba5bffe9ffa0a594457db9ef462d0
Camila Vives <cav@adhoc.com.ar>
2024-09-05 11:54:31 -0300

    [FIX] l10n_ar_ux: fix compute currency rate

--- l10n_ar_ux/models/account_move_line.py
+++ l10n_ar_ux/models/account_move_line.py
@@ -4,7 +4,7 @@ from odoo import models, api
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    @api.depends('move_id.l10n_ar_currency_rate')
+    @api.depends('move_id.computed_currency_rate')
     def _compute_currency_rate(self):
         forced = self.filtered(lambda x: x.move_id.l10n_ar_currency_rate)
         for rec in forced:

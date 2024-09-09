b27a53ffb92143c78b2c19406b331b6844a90a5d
Rocío Vega <rov@adhoc.com.ar>
2024-09-09 14:38:27 +0000

    [FIX] l10n_ar_withholding_ux: Fix in payment receipt pdf creator view when having a signature set for a company
    closes ingadhoc/odoo-argentina#906
    
    Signed-off-by: Camila Vives <cav@adhoc.com.ar>

--- l10n_ar_withholding_ux/__manifest__.py
+++ l10n_ar_withholding_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian withholding UX',
-    'version': "17.0.1.4.0",
+    'version': "17.0.1.5.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

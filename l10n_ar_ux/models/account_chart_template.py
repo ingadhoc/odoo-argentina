0fdb9c473e175e7d0635bd7b970efa062f49c4a7
Camila Vives <cav@adhoc.com.ar>
2024-11-26 15:13:15 -0300

    [IMP] l10n_ar_ux: set bypass to cash journals

--- l10n_ar_ux/models/account_chart_template.py
+++ l10n_ar_ux/models/account_chart_template.py
@@ -14,4 +14,11 @@ class AccountChartTemplate(models.AbstractModel):
         res = super()._load(template_code, company, install_demo)
         if template_code in ('ar_base', 'ar_ex', 'ar_ri'):
             self.env['account.account'].set_non_monetary_tag(company)
+
+            # Dejamos las cuentas de tipo cash con bypass
+            company = self.env.context['allowed_company_ids'][0]
+            cash_journals = self.env['account.journal'].search([('company_id', '=', company), ('type', '=', 'cash')])
+            for journal in cash_journals:
+                journal.inbound_payment_method_line_ids.write({'payment_account_id': journal.default_account_id.id})
+                journal.outbound_payment_method_line_ids.write({'payment_account_id': journal.default_account_id.id})
         return res

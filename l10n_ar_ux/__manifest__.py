96483a4513a26a0078242109776576e7412e287c
Rocío Vega <rov@adhoc.com.ar>
2024-11-27 20:18:05 +0000

    [REM] l10n_ar_ux: Remove features about checks froms this module
    X-original-commit: 8e32be0818ff5f1f12239fa879ebafb5934d1239

--- l10n_ar_ux/__manifest__.py
+++ l10n_ar_ux/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Argentinian Accounting UX',
-    'version': "18.0.1.0.0",
+    'version': "18.0.1.1.0",
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -10,8 +10,6 @@
     'depends': [
         'l10n_ar',
         'account_internal_transfer',
-        'l10n_latam_check',
-        'account_ux',
     ],
     'data': [
         'data/res_currency_data.xml',

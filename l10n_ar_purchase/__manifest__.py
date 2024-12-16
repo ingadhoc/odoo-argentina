b58a68a7751efae227e12d8a736baf378c968830
Juan José Scarafia <jjs@adhoc.com.ar>
2024-12-16 19:08:54 +0000

    [IMP] l10n_ar_purchase: show only vat tax
    Al igual que en facturas y ventas, solo mostramos columna de IVA en reporte argentino
    
    X-original-commit: a02d741345834eee711982dcfe205bebd17f728b

--- l10n_ar_purchase/__manifest__.py
+++ l10n_ar_purchase/__manifest__.py
@@ -1,6 +1,14 @@
 {
+<<<<<<< HEAD
     'name': 'Argentinean Purchase',
     'version': "17.0.1.1.0",
+||||||| parent of ebb1fb9f (temp)
+    'name': 'Argentinian Purchase',
+    'version': "16.0.1.2.0",
+=======
+    'name': 'Argentinian Purchase',
+    'version': "16.0.1.3.0",
+>>>>>>> ebb1fb9f (temp)
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

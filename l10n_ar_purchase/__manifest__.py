2c95607877fbd44817a7acba8b4e656d1e41ef51
Juan José Scarafía <jjs@adhoc.com.ar>
2024-12-23 10:55:39 -0300

    Update __manifest__.py

--- l10n_ar_purchase/__manifest__.py
+++ l10n_ar_purchase/__manifest__.py
@@ -1,12 +1,6 @@
 {
     'name': 'Argentinean Purchase',
-<<<<<<< HEAD
     'version': "18.0.1.0.0",
-||||||| parent of cc67e1ee (temp)
-    'version': "17.0.1.1.0",
-=======
-    'version': "17.0.1.2.0",
->>>>>>> cc67e1ee (temp)
     'category': 'Localization/Argentina',
     'sequence': 14,
     'author': 'ADHOC SA',

58368783ec9f550f87abe2c00a38353b7bc87287
Julia Elizondo <jue@adhoc.com.ar>
2025-01-16 13:41:28 -0300

    [FIX] res_config_settings: merge inherited classes

--- l10n_ar_ux/models/res_config_settings.py
+++ l10n_ar_ux/models/res_config_settings.py
@@ -9,7 +9,12 @@ class ResConfigSettings(models.TransientModel):
     l10n_ar_report_signature = fields.Image('Firma:', related='company_id.l10n_ar_report_signature', readonly=False)
     l10n_ar_report_signed_by = fields.Text('Aclaracion:', related='company_id.l10n_ar_report_signed_by', readonly=False)
     l10n_ar_invoice_report_ars_amount = fields.Boolean(related='company_id.l10n_ar_invoice_report_ars_amount', readonly=False)
-
+    group_include_pending_receivable_documents = fields.Boolean(
+        string="Mostrar comprobantes pendientes en Recibos de Clientes",
+        implied_group='l10n_ar_ux.group_include_pending_receivable_documents',
+        help="Si marca esta opción, cuando se imprima o envíe un Recibo de Clientes, se incluirá"
+        " una sección con todos los Comprobantes abiertos, es decir, que tengan algún saldo pendiente")
+        
     def clean_signature(self):
         self.l10n_ar_report_signature = False
         self.l10n_ar_report_signed_by = False

02ea10fd3575c71685258dcc4e4a94e706d57437
Mariana Santa Clara <mas@adhoc.com.ar>
2025-02-07 11:35:13 -0300

    [FIX] l10n_ar_ux: Fix migration scripts

--- l10n_ar_ux/migrations/17.0.1.1.0/post-migration.py
+++ l10n_ar_ux/migrations/17.0.1.1.0/post-migration.py
@@ -1,11 +1,12 @@
 import logging
 
+from odoo import SUPERUSER_ID, api
 from openupgradelib import openupgrade
 
 logger = logging.getLogger(__name__)
 
 
-@openupgrade.migrate()
-def migrate(env, version):
+def migrate(cr, version):
     logger.info("Forzamos la actualización de la vista res_partner_view.xml en módulo l10n_ar.")
+    env = api.Environment(cr, SUPERUSER_ID, {})
     openupgrade.load_data(env, "l10n_ar", "views/res_partner_view.xml")

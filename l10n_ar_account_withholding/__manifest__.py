8f8c67e8bb2ca887d11405931e88554a0de85a70
Pablo Montenegro <pam@adhoc.com.ar>
2025-02-11 12:47:10 +0000

    [FIX] l10n_ar_account_withholding: astericos al ver clave CIT ARBA
    closes ingadhoc/odoo-argentina#1051
    
    Ticket: 87746
    Signed-off-by: Julia Elizondo <jue@adhoc.com.ar>

--- l10n_ar_account_withholding/__manifest__.py
+++ l10n_ar_account_withholding/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Automatic Argentinian Withholdings on Payments',
-    'version': "17.0.1.3.0",
+    'version': "17.0.1.4.0",
     'author': 'ADHOC SA,Odoo Community Association (OCA)',
     'website': 'www.adhoc.com.ar',
     'license': 'AGPL-3',

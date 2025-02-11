5152c68125d6f4e7457f0c7e05dfd93589042d63
Camila Vives <cav@adhoc.com.ar>
2025-02-11 17:49:28 -0300

    [FIX] l10n_ar_ux: set rounding true for taxes

--- l10n_ar_ux/models/account_tax.py
+++ l10n_ar_ux/models/account_tax.py
@@ -15,3 +15,7 @@ class AccountTax(models.Model):
         for rec in self:
             tag = rec.invoice_repartition_line_ids.tag_ids.filtered('jurisdiction_code')
             rec.jurisdiction_code = tag[0].jurisdiction_code if tag else False
+
+    def compute_all(self, price_unit, currency=None, quantity=1, product=None, partner=None, is_refund=False, handle_price_include=True, include_caba_tags=False, fixed_multiplicator=1):
+        self.env.context = dict(self.env.context, round=True)
+        return super().compute_all(price_unit, currency, quantity, product, partner, is_refund, handle_price_include, include_caba_tags, fixed_multiplicator)

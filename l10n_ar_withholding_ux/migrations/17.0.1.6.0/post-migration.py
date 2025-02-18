05484d21f0eb715c763e3363b0a0488dfc6f2ad0
Camila Vives <cav@adhoc.com.ar>
2025-02-18 12:15:48 -0300

    [FIX] l10n_ar_withholding_ux: adapt pre-migration to new API

--- l10n_ar_withholding_ux/migrations/17.0.1.6.0/post-migration.py
+++ l10n_ar_withholding_ux/migrations/17.0.1.6.0/post-migration.py
@@ -1,11 +1,10 @@
 import logging
 
-from openupgradelib import openupgrade
+from odoo.upgrade import util
 
 logger = logging.getLogger(__name__)
 
 
-@openupgrade.migrate()
-def migrate(env, version):
+def migrate(cr, version):
     logger.info("Forzamos la actualización de la vista res_company_setting.xml en módulo account payment pro")
-    openupgrade.load_data(env, "account_payment_pro", "views/res_company_setting.xml")
+    util.update_record_from_xml(cr, "account_payment_pro.res_config_settings_view_form")

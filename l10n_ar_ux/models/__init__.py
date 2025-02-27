5310a7cccc459e65eb41adb2f63ae3616714582c
Felipe Garcia Suez <feg@adhoc.com.ar>
2025-02-27 15:40:26 -0300

    [REF] l10n_ar_ux: Moved non_monetery logic to account_ux

--- l10n_ar_ux/models/__init__.py
+++ l10n_ar_ux/models/__init__.py
@@ -2,8 +2,6 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from . import account_account
-from . import account_chart_template
 from . import account_move
 from . import res_country_state
 from . import afip_padron

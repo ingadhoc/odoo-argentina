647be8957ff1da11a1f1a5db873746343b3701f3
Julia Elizondo <jue@adhoc.com.ar>
2025-03-12 12:26:33 -0300

    [FIX] l10n_ar_tax: payment reports available in english and spanish

--- l10n_ar_tax/__manifest__.py
+++ l10n_ar_tax/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Automatic Argentinian Withholdings on Payments",
-    "version": "18.0.1.5.0",
+    "version": "18.0.1.6.0",
     "author": "ADHOC SA,Odoo Community Association (OCA)",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",

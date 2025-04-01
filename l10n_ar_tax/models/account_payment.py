0390bac17b1891343fce91a082e4166370556a66
Martin Quinteros <maq@adhoc.com.ar>
2025-04-01 16:12:23 -0300

    [IMP] l10n_ar_tax: POV multiple payment receipt
            This change introduces a new payment receipt report that consolidates multiple payments
            by company, receiver, and secondary currency.
    
            The purpose of this report is to enable the generation of a single,
            consolidated document containing all relevant payment information,
            which can then be sent as an official payment receipt.

--- l10n_ar_tax/models/account_payment.py
+++ l10n_ar_tax/models/account_payment.py
@@ -2,6 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+from collections import defaultdict
+
 from odoo import Command, _, api, fields, models
 from odoo.exceptions import UserError
 
@@ -348,3 +350,24 @@ class AccountPayment(models.Model):
         if self.company_id.country_id.code == "AR" and not self.is_internal_transfer:
             return "l10n_ar_tax.report_payment_receipt_document"
         return super()._get_name_receipt_report(report_xml_id)
+
+    def _get_payment_bundle_key(self):
+        if self.company_id.country_id.code == "AR" and self.env.context.get("print_in_bundles"):
+            return f"{self.company_id.id}-{self.partner_id.id}-{self.payment_type}-{self.currency_id.id if self.other_currency else self.counterpart_currency_id.id}"
+        return self.id
+
+    def _get_payment_bundles(self):
+        """Returns a dictionary of payment bundles, where the key is a tuple
+        of (company_id, partner_id, payment_type, currency_id) and the value
+        is a recordset of account.payment."""
+        bundles = defaultdict(lambda: self.env["account.payment"])
+        for rec in self:
+            bundles[rec._get_payment_bundle_key()] += rec
+        return bundles
+
+    def _select_bundle(self, bundles):
+        """Selects a bundle from the dictionary of payment bundles based on
+        the current record's company_id, partner_id, payment_type, and
+        currency_id."""
+        self.ensure_one()
+        return bundles.get(self._get_payment_bundle_key())

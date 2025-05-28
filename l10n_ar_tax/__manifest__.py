18273e54af64501bfaf5da44bb19f4efe579747e
Pablo Montenegro <pam@adhoc.com.ar>
2025-05-28 09:33:18 -0300

    [IMP] l10n_ar_tax: add company name suffix to withholding/perception taxes on partner form view. Task: 52262

--- l10n_ar_tax/__manifest__.py
+++ l10n_ar_tax/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Automatic Argentinian Withholdings on Payments",
-    "version": "18.0.1.8.0",
+    "version": "18.0.1.9.0",
     "author": "ADHOC SA,Odoo Community Association (OCA)",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",

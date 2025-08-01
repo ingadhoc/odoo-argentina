7df790903302b41ebdb93a3a8d5983f473b9128b
Juan José Scarafia <jjs@adhoc.inc>
2025-08-01 11:12:59 -0300

    [IMP] l10n_ar_tax: update perceptions when needed
    Mejora varios casos de uso, por ejemplo:
    1. si tengo una venta de julio pero que la factura se crea en agosto, las percepciones van a venir calculadas desde julio. Actualizar fecha o validr a factura va a re-computarlas
    2. una factura se pudo crear en julio pero no tener fecha, al validarla en agosto se recomputa porque las alícuotas podrían haber cambiado
    3. duplicar una factura de julio estando en agosto debe recomputar alícuotas
    
    Casos similares ocurren también con clientes que migran y pueden crear facturas de ventas viejas o duplicar facturas viejas que tienen impuestos archivados (el impuesto legacy de partner aliquot)
    
    closes ingadhoc/odoo-argentina#1149
    
    Related: ingadhoc/argentina-sale#250
    Signed-off-by: pablohmontenegro <pam@adhoc.com.ar>

--- l10n_ar_tax/models/account_move.py
+++ l10n_ar_tax/models/account_move.py
@@ -1,10 +1,23 @@
-from odoo import models
+from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
     """Heredamos todos los metodos que de alguna manera llamen a tax.compute_all y les pasamos la fecha"""
 
     _inherit = "account.move"
+
+    perceptions_fiscal_positon = fields.Boolean(
+        compute="_compute_perceptions_fiscal_position",
+    )
+
+    def _compute_perceptions_fiscal_position(self):
+        """
+        Compute if the fiscal position has perceptions.
+        """
+        for move in self:
+            move.perceptions_fiscal_positon = bool(
+                move.fiscal_position_id.l10n_ar_tax_ids.filtered(lambda x: x.tax_type == "perception")
+            )
 
     def _get_tax_factor(self):
         self.ensure_one()
@@ -15,3 +28,23 @@ class AccountMove(models.Model):
         if tax_factor == 1.0 and doc_letter == "B":
             tax_factor = 1.0 / 1.21
         return tax_factor
+
+    def _post(self, soft=True):
+        self.filtered(lambda x: x.perceptions_fiscal_positon and not x.invoice_date).mapped(
+            "invoice_line_ids"
+        )._compute_tax_ids()
+        return super()._post(soft=soft)
+
+    @api.onchange("invoice_date")
+    def _l10n_ar_onchange_invoice_date(self):
+        self.filtered(
+            lambda x: x.is_sale_document(include_receipts=True) and x.perceptions_fiscal_positon and x.state == "draft"
+        ).mapped("invoice_line_ids")._compute_tax_ids()
+
+    def copy(self, default=None):
+        """Re computamos las percepciones al duplicar una factura porque puede ser que la factura venga de otro periodo
+        o por alguna razón las percepciones hayan cambiado
+        """
+        recs = super().copy(default=default)
+        recs._l10n_ar_onchange_invoice_date()
+        return recs

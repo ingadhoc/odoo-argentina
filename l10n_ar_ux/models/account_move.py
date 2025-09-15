0550c7c7a60f97524cdb8d26566e9f2f3d77b16a
Camila Vives <cav@adhoc.com.ar>
2025-09-15 18:40:29 +0000

    [BP][FIX] l10n_ar_ux: also round invoice lines
    This commit is a backport from v18 for this commit:
    1. https://github.com/ingadhoc/odoo-argentina/commit/32f2c2f172e2af9e2a3a4fbe4febe7e5fb28fc3e
    and the following fixes:
    2. https://github.com/ingadhoc/odoo-argentina/commit/a2d9cd3b07fc38ade3c046a436fc3bd675e54eb5
    3. https://github.com/ingadhoc/odoo-argentina/commit/9c7c232265da7456f337e87291686d4a398ac12a
    4. https://github.com/ingadhoc/odoo-argentina/commit/ec03c1e7eda272d3c1dd934a2e6a7debe3f5b3d5
    
    X-original-commit: 3b184eceedfd74f4a3787eb07f4ddbb3a579bff1

--- l10n_ar_ux/models/account_move.py
+++ l10n_ar_ux/models/account_move.py
@@ -2,8 +2,15 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
+<<<<<<< 448c6c2f8cd466df45429ac9db62de782357d547
 from odoo import _, api, models
+||||||| e8305370fc9a1d83f9cc8c6cf520c8426caf50d5
+from odoo import models, fields, api, _
+=======
+>>>>>>> 7189ea612c7ffc9d402f7c0cf23721a4ff758d14
 from odoo.exceptions import UserError
+
+from odoo import _, api, fields, models
 
 
 class AccountMove(models.Model):
@@ -39,11 +46,95 @@ class AccountMove(models.Model):
                 return True
             else:
                 return not (
+<<<<<<< 448c6c2f8cd466df45429ac9db62de782357d547
                     self.company_id.l10n_ar_company_requires_vat
                     and self.partner_id.l10n_ar_afip_responsibility_type_id.code in ["1"]
                     or False
                 )
         return self.l10n_latam_document_type_id.l10n_ar_letter in ["B", "C", "X", "R"]
+||||||| e8305370fc9a1d83f9cc8c6cf520c8426caf50d5
+                    self.company_id.l10n_ar_company_requires_vat and
+                    self.partner_id.l10n_ar_afip_responsibility_type_id.code in ['1'] or False)
+        return self.l10n_latam_document_type_id.l10n_ar_letter in ['B', 'C', 'X', 'R']
+
+    def _post(self, soft=True):
+        """ Estamos sobreescribiendo este método para hacer cosas que en odoo oficial no se puede tanto previo como posterior a la validación de la factura. """
+
+        # estamos haciendo varias cosas acá:
+        # 1. para facturas sin documentos guardamos el rate (en l10n_ar solo se hace para las que usan documentos)
+        # 2. en vez de que el rate se seete luego de postear (que es lo que l10n_ar) lo hacaemos antes para garantizar
+        # que se actualicen los apuntes contables con el rate que efectivamente se va a autilizar
+        # hacemos el hack del +1 porque sin eso no termina de actualizar
+        # el metodo _set_afip_rate super lo llama pero no va a hacer nada porque ya llega con un de
+        # l10n_ar_currency_rate seteado
+        not_use_doc_with_currency_ar_invoices = self.filtered(
+            lambda x: x.company_id.account_fiscal_country_id.code == "AR" and x.is_invoice(include_receipts=True)
+            and x.currency_id != x.company_currency_id and not x.l10n_ar_currency_rate)
+        for rec in not_use_doc_with_currency_ar_invoices:
+            rate = self.env['res.currency']._get_conversion_rate(
+                        from_currency=rec.currency_id,
+                        to_currency=rec.company_id.currency_id,
+                        company=rec.company_id,
+                        date=rec.invoice_date or fields.Date.context_today(rec),
+                    )
+            rec.write({'l10n_ar_currency_rate': rate + 1, 'tax_totals': rec.tax_totals})
+            rec.write({'l10n_ar_currency_rate': rate, 'tax_totals': rec.tax_totals})
+        for line in self.filtered(
+	        lambda x: x.company_id.account_fiscal_country_id.code == "AR"
+	        and x.currency_id != x.company_currency_id).mapped('line_ids').filtered(lambda x:
+	            x.tax_line_id
+	            and x.currency_rate
+	            and not x.currency_id.is_zero(abs(x.amount_currency) / x.currency_rate - abs(x.balance))
+	        ):
+	        balance = line.company_id.currency_id.round(line.amount_currency / line.currency_rate)
+	        line.balance = balance
+        res = super()._post(soft=soft)
+        return res
+=======
+                    self.company_id.l10n_ar_company_requires_vat and
+                    self.partner_id.l10n_ar_afip_responsibility_type_id.code in ['1'] or False)
+        return self.l10n_latam_document_type_id.l10n_ar_letter in ['B', 'C', 'X', 'R']
+
+    def _post(self, soft=True):
+        """ Estamos sobreescribiendo este método para hacer cosas que en odoo oficial no se puede tanto previo como posterior a la validación de la factura. """
+
+        # estamos haciendo varias cosas acá:
+        # 1. para facturas sin documentos guardamos el rate (en l10n_ar solo se hace para las que usan documentos)
+        # 2. en vez de que el rate se seete luego de postear (que es lo que l10n_ar) lo hacaemos antes para garantizar
+        # que se actualicen los apuntes contables con el rate que efectivamente se va a autilizar
+        # hacemos el hack del +1 porque sin eso no termina de actualizar
+        # el metodo _set_afip_rate super lo llama pero no va a hacer nada porque ya llega con un de
+        # l10n_ar_currency_rate seteado
+        not_use_doc_with_currency_ar_invoices = self.filtered(
+            lambda x: x.company_id.account_fiscal_country_id.code == "AR" and x.is_invoice(include_receipts=True)
+            and x.currency_id != x.company_currency_id and not x.l10n_ar_currency_rate)
+        for rec in not_use_doc_with_currency_ar_invoices:
+            rate = self.env['res.currency']._get_conversion_rate(
+                        from_currency=rec.currency_id,
+                        to_currency=rec.company_id.currency_id,
+                        company=rec.company_id,
+                        date=rec.invoice_date or fields.Date.context_today(rec),
+                    )
+            rec.write({'l10n_ar_currency_rate': rate + 1, 'tax_totals': rec.tax_totals})
+            rec.write({'l10n_ar_currency_rate': rate, 'tax_totals': rec.tax_totals})
+
+        ar_invoices = self.filtered(
+            lambda x: x.company_id.account_fiscal_country_id.code == "AR"
+            and x.currency_id != x.company_currency_id
+            and x.is_invoice()
+        )
+        ar_invoice_line_ids = ar_invoices.mapped("invoice_line_ids").ids
+
+        for line in ar_invoices.mapped("line_ids").filtered(
+            lambda x: (x.tax_line_id or x.id in ar_invoice_line_ids)
+            and x.currency_rate
+            and not x.currency_id.is_zero(abs(x.amount_currency) / x.currency_rate - abs(x.balance))
+        ):
+            balance = line.company_id.currency_id.round(line.amount_currency / line.currency_rate)
+            line.balance = balance
+        res = super()._post(soft=soft)
+        return res
+>>>>>>> 7189ea612c7ffc9d402f7c0cf23721a4ff758d14
 
     @api.model
     def _l10n_ar_get_document_number_parts(self, document_number, document_type_code):

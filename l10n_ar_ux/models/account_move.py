3b184eceedfd74f4a3787eb07f4ddbb3a579bff1
Camila Vives <cav@adhoc.com.ar>
2025-09-15 18:40:29 +0000

    [BP][FIX] l10n_ar_ux: also round invoice lines
    This commit is a backport from v18 for this commit:
    1. https://github.com/ingadhoc/odoo-argentina/commit/32f2c2f172e2af9e2a3a4fbe4febe7e5fb28fc3e
    and the following fixes:
    2. https://github.com/ingadhoc/odoo-argentina/commit/a2d9cd3b07fc38ade3c046a436fc3bd675e54eb5
    3. https://github.com/ingadhoc/odoo-argentina/commit/9c7c232265da7456f337e87291686d4a398ac12a
    4. https://github.com/ingadhoc/odoo-argentina/commit/ec03c1e7eda272d3c1dd934a2e6a7debe3f5b3d5
    
    closes ingadhoc/odoo-argentina#1195
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- l10n_ar_ux/models/account_move.py
+++ l10n_ar_ux/models/account_move.py
@@ -2,8 +2,9 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+
+from odoo import _, api, fields, models
 
 
 class AccountMove(models.Model):
@@ -89,15 +90,21 @@ class AccountMove(models.Model):
                     )
             rec.write({'l10n_ar_currency_rate': rate + 1, 'tax_totals': rec.tax_totals})
             rec.write({'l10n_ar_currency_rate': rate, 'tax_totals': rec.tax_totals})
-        for line in self.filtered(
-	        lambda x: x.company_id.account_fiscal_country_id.code == "AR"
-	        and x.currency_id != x.company_currency_id).mapped('line_ids').filtered(lambda x:
-	            x.tax_line_id
-	            and x.currency_rate
-	            and not x.currency_id.is_zero(abs(x.amount_currency) / x.currency_rate - abs(x.balance))
-	        ):
-	        balance = line.company_id.currency_id.round(line.amount_currency / line.currency_rate)
-	        line.balance = balance
+
+        ar_invoices = self.filtered(
+            lambda x: x.company_id.account_fiscal_country_id.code == "AR"
+            and x.currency_id != x.company_currency_id
+            and x.is_invoice()
+        )
+        ar_invoice_line_ids = ar_invoices.mapped("invoice_line_ids").ids
+
+        for line in ar_invoices.mapped("line_ids").filtered(
+            lambda x: (x.tax_line_id or x.id in ar_invoice_line_ids)
+            and x.currency_rate
+            and not x.currency_id.is_zero(abs(x.amount_currency) / x.currency_rate - abs(x.balance))
+        ):
+            balance = line.company_id.currency_id.round(line.amount_currency / line.currency_rate)
+            line.balance = balance
         res = super()._post(soft=soft)
         return res
 

92294df69c4a0d7b3e71f388f14bce6abbf7c4db
Martin Quinteros <maq@adhoc.com.ar>
2025-11-24 00:07:29 -0300

    [IMP] Pyafip So Long, and Thanks for All the Fish (part II)
    	l10n_ar_tax: Arba CIT replace pyafip method.

--- l10n_ar_tax/models/res_company.py
+++ l10n_ar_tax/models/res_company.py
@@ -1,16 +1,14 @@
 import logging
 import warnings
+import xml.etree.ElementTree as ET
+from hashlib import md5
 
+import requests
 from odoo import _, api, models
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-try:
-    from pyafipws.iibb import IIBB
-except ImportError:
-    IIBB = None
-    _logger.info("Ocurrió un problema en la importación de la librería 'pyafipws.iibb'")
 
 
 class ResCompany(models.Model):
@@ -36,46 +34,67 @@ class ResCompany(models.Model):
     @api.model
     def get_arba_login_url(self, environment_type):
         if environment_type == "production":
-            arba_login_url = "https://dfe.arba.gov.ar/DomicilioElectronico/" "SeguridadCliente/dfeServicioConsulta.do"
+            arba_login_url = "https://dfe.arba.gov.ar/DomicilioElectronico/SeguridadCliente/dfeServicioConsulta.do"
         else:
-            arba_login_url = (
-                "https://dfe.test.arba.gov.ar/DomicilioElectronico" "/SeguridadCliente/dfeServicioConsulta.do"
-            )
+            arba_login_url = "https://dfe.test.arba.gov.ar/DomicilioElectronico/SeguridadCliente/dfeServicioConsulta.do"
         return arba_login_url
 
-    def arba_connect(self):
-        """
-        Method to be called
-        """
-        self.ensure_one()
-        cuit = self.partner_id.ensure_vat()
+    def _arba_prepare_file(self, vat, date_from, date_to):
+        qcontext = {
+            "date_to": date_to,
+            "date_from": date_from,
+            "vats": vat if isinstance(vat, list) else [vat],
+        }
+        xml = str(self.env["ir.ui.view"]._render_template("l10n_ar_tax.arba_alicout_query", qcontext)).strip()
+        hash_signature = md5(xml.encode()).hexdigest()
+        filename = "DFEServicioConsulta_%s.xml" % hash_signature
+        return (filename, str(xml).strip(), "text/xml")
 
+    def _arba_cit_parse_response(self, response_xml):
+        root = ET.fromstring(response_xml)
+        if root.tag == "ERROR":
+            error_type = root.find(".//tipoError").text
+            error_code = root.find(".//codigoError").text
+            error_msg = root.find(".//mensajeError").text.replace("<![CDATA[", "").replace("]]/>", "")
+            raise UserError(_("ARBA Error %s(%s): %s") % (error_type, error_code, error_msg))
+        if root.tag == "COMPROBANTE":
+            try:
+                alicouts = {
+                    "numeroComprobante": root.find(".//numeroComprobante").text,
+                }
+                # TODO Por ahora solo retornamos un contribuyente y por retrocompatibilidad
+                # deberiamos a mantenerlo cantidadContribuyentes == 1
+                for contribuyente in root.find(".//contribuyentes").findall("contribuyente"):
+                    alicouts["AlicuotaPercepcion"] = contribuyente.find("alicuotaPercepcion").text
+                    alicouts["AlicuotaRetencion"] = contribuyente.find("alicuotaRetencion").text
+                    alicouts["GrupoPercepcion"] = contribuyente.find("grupoPercepcion").text
+                    alicouts["GrupoRetencion"] = contribuyente.find("grupoRetencion").text
+                return alicouts
+            except Exception as e:
+                _logger.error("Error parsing ARBA response: %s\n %s", e, str(response_xml))
+                raise UserError(_("Error parsing ARBA response: %s") % str(e))
+        _logger.error("Error parsing ARBA response: %s", str(response_xml))
+        raise UserError(_("Error parsing ARBA response"))
+
+    def arba_consultar_contribuyente(self, vat, date_from, date_to):
+        self.ensure_one()
+        self.partner_id.ensure_vat()
         if not self.arba_cit:
             raise UserError(_("You must configure CIT password on company %s") % (self.name))
-
-        try:
-            ws = IIBB()
-            environment_type = self._get_arba_environment_type()
-            _logger.info("Getting connection to ARBA on %s mode" % environment_type)
-
-            # argumentos de conectar: self, url=None, proxy="",
-            # wrapper=None, cacert=None, trace=False, testing=""
-            arba_url = self.get_arba_login_url(environment_type)
-            ws.Usuario = cuit
-            ws.Password = self.arba_cit
-            ws.Conectar(url=arba_url)
-            _logger.info('Connection getted to ARBA with url "%s" and CUIT %s' % (arba_url, cuit))
-        except ConnectionRefusedError:
-            raise UserError(
-                "No se pudo conectar a ARBA para extraer los datos de percepcion/retención."
-                " Por favor espere unos minutos e intente de nuevo. Sino, cargue manualmente"
-                " los datos en el cliente para poder operar (fecha %s)" % self.env.context.get("invoice_date")
-            )
-
-        return ws
-
-    @api.model
-    def _process_message_error(self, ws):
-        message = ws.MensajeError
-        message = message.replace("<![CDATA[", "").replace("]]/>", "")
-        raise UserError(_("Padron ARBA: %s - %s (%s)") % (ws.CodigoError, message, ws.TipoError))
+        environment_type = self._get_arba_environment_type()
+        file = self._arba_prepare_file(vat, date_from, date_to)
+        login_url = self.get_arba_login_url(environment_type)
+        arba_alicout_timeout = int(
+            self.env["ir.config_parameter"].sudo().get_param("l10n_ar_tax.arba_alicout_timeout", default=40)
+        )
+        request_data = {
+            "user": self.partner_id.ensure_vat(),
+            "password": self.arba_cit,
+        }
+        res = requests.post(login_url, data=request_data, files={"file": file}, timeout=arba_alicout_timeout)
+        if res.ok:
+            response_xml = self._arba_cit_parse_response(res.content)
+        else:
+            _logger.error("Error parsing ARBA response: %s\n %s", str(res.text))
+            raise UserError(self.env._("Error parsing ARBA response: %s") % str(res.text))
+        return response_xml

9a3d4660880046163edc6d1f107cc885a14adeb6
Juan José Scarafia <jjs@adhoc.inc>
2025-12-22 14:56:43 -0300

    [FIX] l10n_ar_tax: fix demo and fix odoo pay register wizard
    closes ingadhoc/odoo-argentina#1258
    
    Related: ingadhoc/odoo-argentina-ee#829
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_tax/wizard/__init__.py
+++ l10n_ar_tax/wizard/__init__.py
@@ -4,3 +4,4 @@
 ##############################################################################
 from . import res_config_settings
 from . import account_payment_register
+from . import l10n_ar_payment_register_withholding

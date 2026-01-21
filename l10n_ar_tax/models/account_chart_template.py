63d2dd6eaab9cdadfb81a7f466d1c76d39aad7a9
Juan José Scarafia <jjs@adhoc.inc>
2026-01-21 16:24:55 -0300

    [REF] l10n_ar_ux: deprecate unused tags
    Estas etiquetas ya no las usamos para nada
    
    closes ingadhoc/odoo-argentina#1283
    
    Related: ingadhoc/odoo-argentina-ee#879
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- l10n_ar_tax/models/account_chart_template.py
+++ l10n_ar_tax/models/account_chart_template.py
@@ -4,7 +4,7 @@
 ##############################################################################
 import logging
 
-from odoo import Command, api, models
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 
@@ -110,37 +110,6 @@ class AccountChartTemplate(models.AbstractModel):
         )
         profits_taxes = self.env["account.tax"].with_context(active_test=False).search(profits_domain)
         profits_taxes.l10n_ar_withholding_sequence_id = sequence.id
-
-        # agregado de etiquetas para liquidacion de impuestos sicore
-        sicore_taxes = profits_taxes
-        tag = self.env.ref("l10n_ar_ux.tag_ret_perc_sicore_aplicada", raise_if_not_found=False)
-        if tag:
-            for xml_id in ["ri_tax_percepcion_iva_aplicada", "ri_tax_percepcion_ganancias_aplicada"]:
-                xml_id_percep = "account.%s_%s" % (company.id, xml_id)
-                # en profits_taxes tenemos todas las retenciones, agregamos el impuesto de percepcion de ganancias y de iva
-                tax = self.env.ref(xml_id_percep, raise_if_not_found=False)
-                if tax:
-                    sicore_taxes += tax
-            self.env["account.tax.repartition.line"].search(
-                [("tax_id", "in", sicore_taxes.ids), ("repartition_type", "=", "tax")]
-            ).tag_ids = [Command.link(tag.id)]
-
-        # agregado de etiquetas para liquidacion de impuestos pago IIBB a cuenta (sifere web)
-        # consideramos de IIBB a todo lo que tiene 10n_ar_state_id
-        tag = self.env.ref("l10n_ar_ux.tax_tag_a_cuenta_iibb", raise_if_not_found=False)
-        if tag:
-            domain = [
-                ("repartition_type", "=", "tax"),
-                ("tax_id.company_id", "=", company.id),
-                ("tax_id.l10n_ar_state_id", "!=", False),
-                ("tax_id.country_code", "=", "AR"),
-                "|",
-                ("tax_id.type_tax_use", "=", "purchase"),
-                "&",
-                ("tax_id.type_tax_use", "=", "none"),
-                ("tax_id.l10n_ar_withholding_payment_type", "=", "customer"),
-            ]
-            self.env["account.tax.repartition.line"].search(domain).tag_ids = [Command.link(tag.id)]
 
     def _load(self, template_code, company, install_demo, force_create=True):
         """Luego de que creen los impuestos del archivo account.tax-ar_ri.csv de l10n_ar al instalar el plan de cuentas en la nueva compañìa argentina agregamos en este método las etiquetas que correspondan en los repartition lines."""

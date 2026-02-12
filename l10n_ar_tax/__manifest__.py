fd0b2c749f4aec63c6d59277ef6bb754485395bb
Juan José Scarafia <jjs@adhoc.inc>
2026-02-12 16:55:55 -0300

    [FIX] l10n_ar_tax: fix sincronization of payment to move
    Cambiamos de _prepare_move_liquidity_lines a _prepare_move_lines_per_type para que modificaciones en pago se sincornicen al asiento.
    
    Si bien odoo creó métodos más bajos y heredables mantenemos approach que veníamos haciendo de heredar el más alto (_prepare_move_lines_per_type). En master/20 podríamos evaluar mover odo a pisar los métodos más bajos / particulares
    
    closes ingadhoc/odoo-argentina#1300
    
    closes ingadhoc/odoo-argentina#1303
    
    Related: ingadhoc/account-payment#908
    Related: ingadhoc/account-payment#909
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- l10n_ar_tax/__manifest__.py
+++ l10n_ar_tax/__manifest__.py
@@ -19,11 +19,7 @@
 ##############################################################################
 {
     "name": "Automatic Argentinian Withholdings on Payments",
-<<<<<<< HEAD
-    "version": "19.0.1.4.0",
-=======
-    "version": "18.0.1.24.0",
->>>>>>> 9910e533 ([FIX] account_payment: adapt to new Odoo 18 withholding synchronization)
+    "version": "19.0.1.3.0",
     "author": "ADHOC SA,Odoo Community Association (OCA)",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",

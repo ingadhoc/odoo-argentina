c4f20dfcf812865ac7047359a477a97f5d6b1b31
Pablo Montenegro <pam@adhoc.com.ar>
2026-04-21 12:37:21 +0000

    [IMP] l10n_ar_tax: add monthly cron to clean old padron files.
    1) Add _cron_clean_old_padron_files method to delete old records.
    2) Create cron in data (noupdate="1") with monthly execution.
    3) Add test_cron_deletes_only_old_padrons test ( odoo-argentina/l10n_ar_tax/tests/test_padron_cleanup_cron.py ) to verify old records are deleted after cron execution.
    Task Adhoc side: 65316
    
    closes ingadhoc/odoo-argentina#1381
    
    X-original-commit: 7098240c849489c6e8b392f61c0b77811ae2a20e
    Signed-off-by: Katherine Zaoral - kz (#l10n) <kz@adhoc.com.ar>
    Signed-off-by: Pablo Montenegro - pam (#l10n) <pam@adhoc.com.ar>

--- l10n_ar_tax/tests/__init__.py
+++ l10n_ar_tax/tests/__init__.py
@@ -2,3 +2,4 @@ from . import test_arba
 from . import test_withholding_thresholds
 from . import test_payment_withholding_multimoneda
 from . import test_payment_withholding_checks_multimoneda
+from . import test_padron_cleanup_cron

fc9382e0cd301efe58ab1edbf9f1c63b9a1b789a
Julia Elizondo <jue@adhoc.com.ar>
2026-04-21 16:03:10 +0000

    [ADD] l10n_ar_tax: added test cases

--- l10n_ar_tax/tests/__init__.py
+++ l10n_ar_tax/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_arba
+from . import test_map_tax_fiscal_position
 from . import test_withholding_thresholds
 from . import test_payment_withholding_multimoneda
 from . import test_payment_withholding_checks_multimoneda

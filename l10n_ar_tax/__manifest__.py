54c99013c0e60d6b87a4447aad4ba7bd51b1ccd4
roboadhoc <roboadhoc@adhoc.com.ar>
2026-04-24 17:39:54 +0000

    [BOT] Bump version: l10n_ar_tax 19.0.1.14.0
    Merged: ingadhoc/odoo-argentina#1383

--- l10n_ar_tax/__manifest__.py
+++ l10n_ar_tax/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Automatic Argentinian Withholdings on Payments",
-    "version": "19.0.1.13.0",
+    "version": "19.0.1.14.0",
     "author": "ADHOC SA,Odoo Community Association (OCA)",
     "website": "www.adhoc.com.ar",
     "license": "AGPL-3",

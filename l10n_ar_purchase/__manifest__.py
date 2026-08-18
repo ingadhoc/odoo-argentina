109bb62881372a34fdde595ee05e9fe7eb640afc
roboadhoc <roboadhoc@adhoc.com.ar>
2026-08-18 21:27:12 +0000

    [BOT] Bump version: l10n_ar_purchase 19.0.1.2.0
    Merged: ingadhoc/odoo-argentina#1541

--- l10n_ar_purchase/__manifest__.py
+++ l10n_ar_purchase/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Argentinean Purchase",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "category": "Localization/Argentina",
     "sequence": 14,
     "author": "ADHOC SA",
